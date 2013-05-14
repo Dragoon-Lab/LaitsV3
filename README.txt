8c094f6591fbce63f89905dd705f0b27722f233e
Reid Joiner <reidjoiner@en4046191.dhcp.asu.edu>
2013-05-14 09:43:01 -0700

    Copyright and LGPL added
    Copyright statement and LGPL added to files with Ram as documented
    author.

---
+++ README.txt
@@ -0,0 +1,14 @@
+
+LaitsV3
+Version 3 of LAITS Project with jGraph
+(c) 2013, Arizona Board of Regents for and on behalf of Arizona State University.
+
+Requires command line arguments in to run locally from NetBeans. To set up command line arguments in NetBeans, right-click the project, select "Configuration" > "Customize", select the "Run" category, and enter into the Arguments line in this format:
+
+<username> <mode> <problemid>
+
+Currently accepted modes are STUDENT and AUTHOR. Problemid is the problem number you wish to run. Example command line argument:
+
+developer1 STUDENT 105
+
+Currently valid problem ids: 74, 79, 80, 92 - 102, 105 -115, 201 - 205
--- src/edu/asu/laits/gui/menus/ModelMenu.java
+++ src/edu/asu/laits/gui/menus/ModelMenu.java
@@ -1,3 +1,20 @@
+/** (c) 2013, Arizona Board of Regents for and on behalf of Arizona State University.
+ * This file is part of LAITS.
+ *
+ * LAITS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LAITS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LAITS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.asu.laits.gui.menus;
 
 import edu.asu.laits.editor.ApplicationContext;
