ddef882e670515e263fb0faae8db1fbbb1ea781f
rjoiner1 <rjoiner1@10.142.248.73>
2013-06-05 12:35:29 -0700

    Solution Node INCREASE_AND_DECREASE

--- src/edu/asu/laits/model/SolutionNode.java
+++ src/edu/asu/laits/model/SolutionNode.java
@@ -77,6 +77,8 @@ public class SolutionNode {
             nodePlan = Plan.INCREASE;
         else if(planText.equalsIgnoreCase("said to decrease"))
             nodePlan = Plan.DECREASE;
+        else if(planText.equalsIgnoreCase("said to both increase and decrease"))
+            nodePlan = Plan.INCREASE_AND_DECREASE;
     }
     
     public void setInputNodes(List<String> nodeList){
