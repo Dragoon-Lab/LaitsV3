743070bebc15df54ae339019e545e4df0d159327
Brett van de Sande <bvds@asu.edu>
2013-06-19 17:05:45 -0700

    Fix TaskName in wrong place

--- www/problems/splitter.pl
+++ www/problems/splitter.pl
@@ -12,13 +12,18 @@ use warnings;
 @ARGV or die "Input file required as command-line parameter\n";
 
 my $out;
+my $tn;
 
 while (<>) {
   if ( /(\d+)\t(.*?)\t(.*)/g ) {
     open $out, '>', "$1.xml" or die $!;
     select $out;
-    chomp $3;
-    $_=$3 . "\n<TaskName>$2</TaskName>";
+    $tn=$2;
+    $_=$3;
+  }
+  if(/^<Task /){
+      chomp;
+      $_=$_ . "\n  <TaskName>$tn</TaskName>\n"; 
   }
   s/\r\n$/\n/g;
   print $_ if $out;
