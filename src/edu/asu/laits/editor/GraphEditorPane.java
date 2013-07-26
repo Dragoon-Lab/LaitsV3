ab3b3ca68bf98925f32fba55d2d59a52b646c326
rjoiner1 <rjoiner1@10.142.248.73>
2013-07-26 09:08:13 -0700

    Help Bubble work

--- src/edu/asu/laits/editor/GraphEditorPane.java
+++ src/edu/asu/laits/editor/GraphEditorPane.java
@@ -146,7 +146,7 @@ public class GraphEditorPane extends JGraph {
         this.setPortsVisible(false);
         
         init();
-        loadSession();
+        //loadSession();
         
         setGridProps();
         
