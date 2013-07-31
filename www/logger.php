fa8031555a9f281d7a4fd6737f9c0991f3c12baf
Brett van de Sande <bvds@asu.edu>
2013-07-31 14:25:32 -0700

    update image in jared and oprah problems in logger.php, only look at location when needed.

--- www/logger.php
+++ www/logger.php
@@ -10,9 +10,9 @@ $date = mysqli_real_escape_string($mysqli, $_GET['date']);
 $logger = mysqli_real_escape_string($mysqli, $_GET['logger']);
 $level = mysqli_real_escape_string($mysqli, $_GET['level']);
 $msg = mysqli_real_escape_string($mysqli, $_GET['msg']);
-$location = mysqli_real_escape_string($mysqli, $_GET['location']);
 
 if($logger == "DevLogs"){
+  $location = mysqli_real_escape_string($mysqli, $_GET['location']);
   $mysqli->query("INSERT INTO dev_logs VALUES ('$user','$date','$location','$level','$msg')");
 }else{
   $mysqli->query("INSERT INTO activity_logs VALUES ('$user','$date','$level','$msg')");
