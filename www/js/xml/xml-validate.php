74f79a134d64c857eefee3d2862ba0fa83c9accb
sbstron <sbstron@en4085136.dhcp.asu.edu>
2013-10-24 15:40:47 -0700

    Update to XML schema

--- www/js/xml/xml-validate.php
+++ www/js/xml/xml-validate.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $xml = new DOMDocument();
-$xml->load('http://localhost/laits/xml-test.xml'); // Or load if filename required
-if (!$xml->schemaValidate('http://localhost/laits/dragoon_schema.xsd')) // Or schemaValidateSource if string used.
+$xml->load('http://localhost/laits/js/xml/xml-test.xml'); // Or load if filename required
+if (!$xml->schemaValidate('http://localhost/laits/js/xml/dragoon_schema.xsd')) // Or schemaValidateSource if string used.
 {    echo "You have an error in the XML file";
 }else{
     echo "XML validated";
