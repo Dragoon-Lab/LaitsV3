801cc5e83825460ce49ec0303df439a2e68f5867
Brandon Strong <sbstron@en4085136.dhcp.asu.edu>
2013-11-18 16:08:28 -0700

    remove file and update comment

--- www/db-research-login.php
+++ www/db-research-login.php
@@ -3,10 +3,10 @@
   // This is meant to be require'd by scripts that 
   // access the database.
   
-  // To log into database, create file db_user_password in 
+  // To log into database, create file db_research_user_password in 
   // project root directory containing username, password
   // and database name.  Then "chmod 600 db_user_password"
-$myFile = "../db_user_password";
+$myFile = "../db_research_user_password";
 $fh = fopen($myFile, 'r') or die("Could not find password file");
 $dbuser = chop(fgets($fh));
 $dbpass = chop(fgets($fh));
