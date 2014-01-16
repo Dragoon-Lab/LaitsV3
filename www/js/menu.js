8ad59c13cd53486ee7c01938e2341290bff5b36d
Brett van de Sande <bvds@asu.edu>
2014-01-15 23:09:38 -0700

    set-up for buttons and menus working.

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,18 +1,37 @@
 /*global define*/
 define([
-	 "dojo/dom",
-	 "dojo/domReady!"
-],function(dom){
-  // This runs after widgets have been set up (maybe domReady! is sufficient?)
-  ready(function(){
-	  var buttons = {
-	    "createNodeButton":function(){
-	      console.log("Create node button pressed.");
-	    }
-	  };
-	  // Connect buttons to handlers
-	  for(var button in buttons){
-	    
-	  }
+    "dojo/dom",
+    "dojo/ready",
+    "dijit/registry",
+    "dojo/on"
+],function(dom, ready, registry, on){
+
+    // List of handlers
+    var buttons = {
+	"createNodeButton":function(){
+	    console.log("Create node button pressed.");
 	}
+    };
+
+    // Connect buttons to handlers
+    // Logging could be put in here or above.
+    // This runs after widgets have been set up.
+    ready(function(){
+	var i=0;
+	  for(var button in buttons){
+              // If I use registry.byId(), then the function is called twice.
+	      var o = dom.byId(button);
+	      if(o){
+                  console.log("wiring up ",button,", widget=",o);
+		  // This is a work-around for getting a button to work inside a MenuBar
+		  // Otherwise, there is an error message.
+		  registry.byId(button)._setSelected = function(arg){
+		      console.log("_setSelected called with ",arg);
+		  };
+		  on(o,"click",buttons[button]);
+	      } else {
+		  console.warn("Can't find menu item ",o);
+	      }
+	  }
+	});
 });
