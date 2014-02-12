d10f2c1f2797cf21c8bb5229498e341d2acdca8d
Pradeep <chaudharipradeep9@gmail.com>
2014-02-12 14:25:15 -0700

    modified controller - shows Node Editor Dijit Dialog

--- www/js/examples/controller/controller.js
+++ www/js/examples/controller/controller.js
@@ -3,7 +3,7 @@
 *        -  Delegate Event Call Backs 
 * 	     -  Loading AMD UI 
 */
-define(['dojo/_base/declare','dojo/dom','dojo/on'], function(declare,dom,on) {
+define(['dojo/_base/declare','dojo/dom','dojo/on','dijit/registry'], function(declare,dom,on,registry) {
 
 	return declare(null, {
 
@@ -18,7 +18,7 @@ define(['dojo/_base/declare','dojo/dom','dojo/on'], function(declare,dom,on) {
 	initHandles:function(){
 	
 			//fetch Node Editor Element Ids
-			var nodeIds = ["population","",""]; 
+			var nodeIds = ["population","growth","grate"]; 
 	
 			/*
 			*	Node Editor Handles
@@ -32,14 +32,15 @@ define(['dojo/_base/declare','dojo/dom','dojo/on'], function(declare,dom,on) {
 			on(done, 'click', null);
 			on(plus, 'click', null);
 			
-			var element = dom.byId(nodeIds[0]);
-			on(element,'click',this.showNodeEditor);
-			
+			for(id in nodeIds){
+				var element = dom.byId(nodeIds[id]);
+				on(element,'click',this.showNodeEditor);
+			}
 	},
 	
 	//show node editor
 	showNodeEditor : function(){
-		var nodeeditor = dom.byId('nodeeditor');
+		var nodeeditor = registry.byId('nodeeditor');
 		nodeeditor.show();
 	}
 	
