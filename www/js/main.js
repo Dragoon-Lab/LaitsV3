e3cc60cde4dade62868d3083cdde186c551b3ae3
Brett van de Sande <bvds@asu.edu>
2014-02-11 23:09:33 -0700

    Move set up for solution chart out of index.html to js/main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,15 @@
 /* global define */
 define([
+    "dojo/dom",
+    "dojo/on",
     "dojo/io-query",
-    "dragoon/load-save"
-       ],function(ioQuery, loadSave, model){ 
+    "dojo/ready",
+    "./load-save",
+    "./model",
+    "./RenderGraph", "./RenderTable"
+],function(dom, on, ioQuery, ready, loadSave, model, Graph, Table){ 
     console.log("load main.js");
-   
+    
     // Get session parameters
     var query={};
     if(window.location.search){
@@ -14,10 +19,50 @@ define([
         console.warn("Dragoon log files won't work since we can't set up a session.");
 	console.error("Function called without arguments");
     }
-
+    
     // Start up new session and get model object from server
     var session = new loadSave(query);	   
     session.loadProblem(query).then(function(solutionGraph){
 	console.info("Have solution: ", solutionGraph);
-    });
+	
+	/*
+	 Make model solution plot using dummy data. 
+	 This should be put in its own module.
+	 */
+	
+	ready(function(){
+	    // dummy parameter to be passed to graph class
+	    var inputParam = 3;
+	    // values of parameters
+	    var paramValue = ['A','B','C'];
+	    var slider = new Array();
+	    var button = dom.byId("graphButton");
+	    
+	    // instantiate graph object
+	    var graph = new Graph(inputParam,paramValue);
+	    
+	    // show graph when button clicked
+	    on(button,"click",function(){
+		console.debug("button clicked");	   
+            graph.show();
+	    }); 
+	    
+	    //dummy parameter to be passed to graph class
+	    inputParam = 5;
+	    // values of parameters
+	    paramValue = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+	    var tableHeader = ['time','Param1','Param2','Param3','Param4'];
+	    slider = new Array();
+	    button = dom.byId("tableButton");
+	
+	    // instantiate graph object
+	    var table = new Table(inputParam,tableHeader,paramValue);
+	
+	    // show graph when button clicked
+	    on(button,"click",function(){        	
+		console.debug("table button clicked");
+		table.show();
+	    }); 
+	});
+    });    
 });
