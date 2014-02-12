e54177d69c3f620930c708507e51266bd087b5cf
reidjoiner <joinerr@gmail.com>
2014-02-12 12:41:34 -0700

    Bugfix for create_database.sql

--- create-database.sql
+++ create-database.sql
@@ -1,11 +1,10 @@
 
---
--- Table structure for table `sessions`
---
-
+DROP TABLE IF EXISTS `step`;
+DROP TABLE IF EXISTS `solutions`;
 DROP TABLE IF EXISTS `session`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `session` (
   `session_id` varchar(50) NOT NULL,
   `mode` varchar(20) NOT NULL,
@@ -17,14 +16,6 @@ CREATE TABLE `session` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- All logging goes in table `step`
---
-
-DROP TABLE IF EXISTS `step`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `step` (
   `tid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `session_id` varchar(50) NOT NULL,
@@ -34,16 +25,10 @@ CREATE TABLE `step` (
   KEY (`session_id`),
   CONSTRAINT `fk_session_id` FOREIGN KEY (`session_id`) REFERENCES `session` (`session_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+USE test;
 --
 -- Table structure for table `solutions`
 --
-/*
-  the share and deleted bits are set here, rather than in the sessions table
-  since they are most naturally set during the autosave process.
-*/
-
-DROP TABLE IF EXISTS `solutions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `solutions` (
@@ -56,3 +41,11 @@ CREATE TABLE `solutions` (
   CONSTRAINT `fk_sesssion_id` FOREIGN KEY (`session_id`) REFERENCES `session` (`session_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+
+
+--  the share and deleted bits are set here, rather than in the sessions table
+--  since they are most naturally set during the autosave process.
+
