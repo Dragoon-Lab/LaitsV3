2a0bf6a8bc6d4c1e7bbb54a0b65a87d846b438e6
Deepak Bhosale <dbhosale@asu.edu>
2014-02-14 16:48:09 -0700

    main.js modified for calculations from given model

--- www/js/main.js
+++ www/js/main.js
@@ -7,7 +7,8 @@ define([
     "./load-save",
     "./model",
     "./RenderGraph", "./RenderTable", "./wraptext",
-     "./controller"
+     "./controller",
+	 "../www/math-parser/parser"
 ],function(dom, on, ioQuery, ready, loadSave, model, Graph, Table, wrapText, controller){ 
     console.log("load main.js");
     
@@ -29,6 +30,88 @@ define([
 
 	var givenModel = new model();
 	givenModel.loadModel(solutionGraph);
+	
+	
+	/*
+	@author: Deepak
+	@brief: code for calculating the given and student solution.This code should be moved to calculatin.js
+	*/
+	
+	
+	
+	var i=0;
+	//get all nodes of given model
+	var givenModelNodes;
+	//get time parameters
+	var startTime,endTime,timeStep;
+	var expr,variable;
+	//use this object to store current key/value pair of value of nodes e.g{id1:100,id2:200}
+	var currentNodeValues = {};
+	var nextNodeValues = {};
+	//create this object to store key/value pair of node equations
+	var nodeEquations = {};
+	
+	
+	
+	givenModelNodes = givenModel.getNodes();
+	
+	startTime = givenModel.getStartTime();
+	endTime = givenModel.getEndTime();
+	timeStep = givenModel.getTimeStep();
+	
+	//this is a function used to find initial values of node of type function
+	var calcNULLNodeValue = function(nodeID)
+	{
+		//object for storing values of variables to be passed to expression
+		var _exprValues = {};
+		var _expr,_variable,_k,_value;
+		_expr = Parser.parse(nodeEquations[nodeID]);
+		_variable = _expr.variables();
+		for(_k=0;_k<_variable.length;_k++)
+		{
+			if(currentNodeValues[_variable[_k]] == null)
+			{
+					calcNULLNodeValue(_variable[_k]);
+			}
+			_exprValues[_variable[_k]] = currentNodeValues[_variable[_k]];
+		}
+		_value = _expr.evaluate(_exprValues);
+		currentNodeValues[nodeID] = _value;
+		
+		return _value;
+		
+	};
+	
+	//get all node equations. Put this code in a function getNodEquation(givenModelNodes)
+	for(i=0;i<givenModelNodes.length;i++)
+	{
+		nodeEquations[givenModelNodes[i].ID] = givenModel.getNodeEquation(givenModelNodes[i].ID);
+	}
+	
+	//put this code in a function 'getInitialNodeValues(givenModelNodes)' to setup 'currentNodeValues' variable above to initial values
+	for(i=0;i<givenModelNodes.length;i++)
+	{
+		currentNodeValues[givenModelNodes[i].ID] = givenModel.getNodeInitial(givenModelNodes[i].ID);
+	}
+	
+	
+	for(j=0;j<givenModelNodes.length;j++)
+	{
+		if(currentNodeValues[givenModelNodes[j].ID] == null)
+		{
+			calcNULLNodeValue(givenModelNodes[j].ID);
+		}
+	}
+		
+	//put this code in function calculateStepValues()
+	for(i=1;i<(endTime-startTime)/timeStep;i++)
+	{
+		for(j=0;j<givenModelNodes.length;j++)
+		{
+			calcNULLNodeValue(givenModelNodes[j].ID);
+		}
+	}
+
 
 	/*
 	 start up controller
