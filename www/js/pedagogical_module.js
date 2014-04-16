b0de22547a1f3dfad801177f266b9a1cee73822b
dAvE <groversachin.211@gmail.com>
2014-04-16 12:40:49 -0700

    merged code with master and my initial commit, its still incomplete

--- www/js/pedagogical_module.js
+++ www/js/pedagogical_module.js
@@ -473,7 +473,10 @@ define([
              Note that I haven't set correct-value.  For most controls, it should be set
              */
             if (this.logging) {
-                this.logging.log('solution-step', {node: studentID, type: nodePart, value: answer, checkResult: interpretation});
+                if(interpretation === 'correct' || interpretation === 'optimal')
+                this.logging.log('solution-step', {node: studentID, name: this.model.student.getName(givenID), type: nodePart, value: answer, checkResult: 'CORRECT', order : interpretation});
+                else
+                    this.logging.log('solution-step', {node: studentID, name: this.model.student.getName(studentID), type: nodePart, value: answer, checkResult: 'INCORRECT', order : interpretation});
             }
             console.log("*****\n", interpretation);
             return interpretation;
