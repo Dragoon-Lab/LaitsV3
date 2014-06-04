89494c0e9b0315f26fc77b29bfe1fdafe5e1d56b
jingxian <jfeng34@asu.edu>
2014-06-04 14:41:32 -0700

    Pop-out window for intro video done.

--- www/js/main.js
+++ www/js/main.js
@@ -234,8 +234,12 @@ define([
                     name: "intro-video"
                 });
 		// It would be better to open a dialog box and 
-		// have an embedded video.
-		window.open("https://www.youtube.com/watch?v=gsrM07XfABk");
+		// have an embedded video. (Done)
+            //"newwindow": the pop-out window name, not required, could be empty
+            //"height" and "width": pop-out window size
+            //Other properties could be changed as the value of yes or no
+		window.open("https://www.youtube.com/watch_popup?v=gsrM07XfABk","newwindow",
+            "height=400, width=600, toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no");
 	    });
 
             /*
