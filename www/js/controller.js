bac01e0187c24992e328c45cb96044303d10caff
Brett van de Sande <bvds@asu.edu>
2014-06-06 16:47:30 -0700

    Undo values in name and description fields when closing node editor in AUTHOR mode.

--- www/js/controller.js
+++ www/js/controller.js
@@ -250,6 +250,18 @@ define([
                 w.set("status", '');  // remove colors
             }
 
+	    // Undo Name value (only in AUTHOR mode)
+	    if(this.controlMap.name){
+		var name = registry.byId(this.controlMap["name"]);
+		name.set("value", "");
+	    }
+
+	    // Undo Description value (only needed in AUTHOR mode)
+	    if(this.controlMap.description){
+		var description = registry.byId(this.controlMap.description);
+		description.set("value", "");
+	    }
+
             // Undo any initial value
             var initial = registry.byId(this.controlMap["initial"]);
             initial.set("value", "");
