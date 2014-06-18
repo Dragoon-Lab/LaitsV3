0429a8ff1e44072ebd744f1248391961577e93c6
Haoxuan Wu <hw399@cornell.edu>
2014-06-18 14:34:35 -0700

    perfect node fix

--- www/js/controller.js
+++ www/js/controller.js
@@ -917,7 +917,7 @@ define([
         },
 
 		// Stub to be overwritten by student or author mode-specific method.
-		colorNodeBorder: function(nodeID){
+		colorNodeBorder: function(nodeID, bool){
 			console.log("colorNodeBorder stub called");
 		}
 
