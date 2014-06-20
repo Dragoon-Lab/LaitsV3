7ea656fc9f327bc9655663affadb4a27c1f8a552
jingxian <jfeng34@asu.edu>
2014-06-20 14:39:16 -0700

    Changed the time significant num to 4

--- www/js/RenderTable.js
+++ www/js/RenderTable.js
@@ -122,7 +122,7 @@ define([
         
             for(var i=0; i<solution.times.length; i++){
 		tableString += "<tr>";
-		tableString += "<td align='center'>" + solution.times[i].toPrecision(3) + "</td>";
+		tableString += "<td align='center'>" + solution.times[i].toPrecision(4) + "</td>";
 		//set values in table according to their table-headers
 		array.forEach(solution.plotValues, function(value){
                     tableString += "<td align='center'>" + value[i].toPrecision(3) + "</td>";
