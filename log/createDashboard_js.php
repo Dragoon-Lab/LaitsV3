d364bddd83c259efbc086aaa471a42cdfb87e90d
dAvE <groversachin.211@gmail.com>
2014-07-08 16:00:50 -0700

    Dashboard commit testing going on

---
+++ log/createDashboard_js.php
@@ -0,0 +1,147 @@
+<?php
+	include "logAnalysis.php";
+	include "logProblemObject.php";
+
+	class Dashboard{
+		public $al;
+
+		function __construct($con){
+			$this->al = new AnalyzeLogs($con);
+		}
+
+		function createDashboard($section, $mode, $user, $problem, $fromTime, $fromDate, $toTime, $toDate){
+			$query = $this->$getQuery($section, $mode, $user, $problem, $fromTime, $fromDate, $toTime, $toDate);
+			$result = $al->getResults($query);
+			$objects = $this->$parseMessages($result);
+			return $objects;
+		}
+
+		function getQuery($section, $mode, $user, $problem, $fromTime, $fromDate, $toTime, $toDate){
+			var $userString = "AND uesr = '".$user."' ";
+			var $problemString = "AND problem = '".$problem."' ";
+			var $queryString = "SELECT tid, session.session_id, user, problem, time, method, message, author from session JOIN step ON session.session_id = step.session_id where section = '".$section."' AND time >= '".$fromDate." ".$fromTime."' AND method != 'client-message' ".(!empty($user)?$userString:"").(!empty($problem) ?$problemString:"")."AND time <= '".(!empty($toDate)?$toDate:$fromDate)." ".$toTime."' AND mode = '".$mode."' ".($user != ''?$userString:'')."ORDER BY user asc, problem_name asc, tid asc;";
+
+			return $queryString;
+		}
+
+		function parseMessages($result){
+			var $resetVariables = true;
+			var $sessionTime, $outOfFocusTime, $wastedTime;
+			var $oldRow, $method, $oldMessage, $newMessage;
+			var $row, $oldSession, $newSession;
+			var $upObject, $currentNode;//up stands for user-problem
+			var $problemReOpen;
+			var $objectArray = array();
+			var $propertyStartTime;
+			while($row = $result->mysqli_fetch($result)){
+				if($resetVariables){
+					$sessionTime = 0, $outOfFocusTime = 0, $wastedTime=0;
+					$problemReOpen = 1;
+					$oldRow = $row;
+					$upObject = new logProblemObject();
+					$upObject->setSessionRunning(true);
+				}
+				$resetVariables = false;
+				$method = $row['method'];
+				$newMessage = json_decode($row['message'], true);
+				$oldMessage = json_decode($oldRow['message'], true);
+				$oldSession = $oldRow['session'];
+				$newSession = $row['session'];
+
+				var $stepTime = $newMessage['time'] - $oldMessage['time'];
+				if($oldSession != $newSession){
+					//this means either the problem was opened again or their is a new user problem combination.
+					if(($oldRow['user'] == $row['user']) && ($oldRow['problem'] == $row['problem'])){
+						$stepTime = 0;
+						$upObject->setSessionRunning(true);
+						$problemReOpen +=1;
+					} else {
+						$resetVariables = true;
+						array_push($objectArray, $upObject);
+					}
+				}
+				if($stepTime > $al->getActionTime() && $method != "window-focus"){
+					$timeWasted += $stepTime;
+				}
+				$sessionTime += $stepTime;
+
+				if($method === "start-session"){
+					$upObject->setProblem($row['problem']);
+				} else if($method === "ui-action"){
+					var $type = $newMessage['type'];
+					if($type === "menu-choice"){
+						//a new node created
+						var $name = $newMessage['name'];
+						if($name === 'create-node'){
+							if(!isset($currentNode)){
+								$currentNode = new Node();
+							} else {
+								$upObject->setNodes($currentNode);
+								$currentNode = new Node();
+							}
+						} else if($name === "graph-button"){
+							$upObject->setProblemComplete($row['problemComplete']);
+						} else if($name === "done-button"){
+							$upObject->setProblemComplete($row['problemComplete']);
+							$upObject->setSessionRunning(false);
+						}
+						$propertyStartTime = $newMessage['time'];
+					} else if($type === "close-dialog-box"){
+						if(isset($currentProerty)){
+							$currentNode->setProperties($currentProperty);
+						}
+						$upObject->setNodes($currentNode);
+						$currentNode = null;
+					} else if($type === "node-delete"){
+						$currentNode->setNodeExist(false);
+					} else if($type === "open-dialog-box"){
+						if(array_key_exists('node', $newMessage)){
+							//node reopened
+							$currentNode = $upObject->getNodeFromName($newMessage['node']);
+							$currentNode->setOpenTimes(($currentNode->getOpenTimes())+1);
+						}
+					}
+				} else if($method === "solution-step"){
+					var $type = $newMessage['type'];
+					if($type === "solution-check"){
+						var $currentProperty = new Property();
+						$currentProperty->setName($newMessage['property']);
+						var $checkResult = $newMessage['checkResult'];
+						$currentProperty->setStatus($checkResult);
+						$currentProperty->setCorrectValue($newMessage['correctValue']);
+
+						if($checkResult === "CORRECT"){
+							$currentProperty->setTime($newMessage['time']-$propertyStartTime);
+							$propertyStartTime = $newMessage['time'];
+							$currentNode->setProperties($currentProperty);
+							$currentProperty = null;
+						} else if($checkResult === "INCORRECT"){
+							$currentProperty->setAnswers($newMessage['value']);
+						}
+					}
+				} else if($method === "window-focus"){
+					if($type === "in-focus"){
+						//window came back in focus
+						$outOfFocusTime += $stepTime; // as previous message will be for out of focus.
+					}
+				}
+
+				if($resetVariables){
+					$upObject->setWastedTime($wastedTime);
+					$upObject->setTotalTime($sessionTime);
+					$upObject->setOutOfFocusTime($outOfFocusTime);
+					$upObject->setOpenTimes($problemReOpen);
+					array_push($objectArray, $upObject);
+				}
+			}
+			$upObject->setWastedTime($wastedTime);
+			$upObject->setTotalTime($sessionTime);
+			$upObject->setOutOfFocusTime($outOfFocusTime);
+			$upObject->setOpenTimes($problemReOpen);
+			array_push($objectArray, $upObject);
+
+			return $objectArray;
+		}
+	}
+
+?>
---
+++ log/logProblemObject.php
@@ -0,0 +1,201 @@
+<?php
+	class UserProblemObject{
+		private $problem;
+		private $user;
+		private $totalTime; // total time for which the session is running
+		private $outOfFocusTime; // time when the window was out of focus
+		private $wastedTime; // for a long time when nothing was done, no logs for 420 seconds.
+		private $problemComplete;
+		private $problemReOpen;
+		private $sessionRunning;
+		private $nodes = array();
+
+		//getter setters for each
+		function setProblem($value){
+			$this->problem = $value;
+		}
+
+		function getProblem(){
+			return $this->problem;
+		}
+
+		function setUser($value){
+			$this->user = $value;
+		}
+
+		function getUser(){
+			return $this->user;
+		}
+
+		function setTotalTime($value){
+			$this->totalTime = $value;
+		}
+
+		function getTotalTime(){
+			return $this->totalTime;
+		}
+
+		function setOutOfFocusTime($value){
+			$this->outOfFocusTime = $value;
+		}
+
+		function getOutOfFocusTime(){
+			return $this->outOfFocusTime;
+		}
+
+		function setWastedTime($value){
+			$this->wastedTime = $value;
+		}
+
+		function getWastedTime(){
+			return $this->wastedTime;
+		}
+
+		function setProblemComplete($value){
+			$this->problemComplete = $value;
+		}
+
+		function getProblemComplete(){
+			return $this->problemComplete;
+		}
+
+		function setProblemReOpen($value){
+			$this->problemReOpen = $value;
+		}
+
+		function getProblemReOpen(){
+			return $this->problemReOpen;
+		}
+
+		function setSessionRunning($value){
+			$this->sessionRunning = $value;
+		}
+
+		function getSessionRunning(){
+			return $this->sessionRunning;
+		}
+
+		function setNodes($value){
+			array_push($this->nodes, $value);
+		}
+
+		function getNodes(){
+			return $this->nodes;
+		}
+
+		function getNodeFromName($name){
+			var $allNodes = this->getNodes();
+			var $resultNode = null;
+			var $node;
+			foreach($allNodes as $node){
+				if($name == $node->getName()){
+					$resultNode = $node;
+					break;
+				}
+			}
+
+			return $resultNode;
+		}
+	}
+
+	class Node{
+		private $name;
+		private $nodeExist;
+		private $openTimes;
+		private $properties = array();
+
+		function setName($value){
+			$this->name = $value;
+		}
+
+		function getName(){
+			return $this->name;
+		}
+
+		function setNodeExist($value){
+			$this->nodeExist = $value;
+		}
+
+		function getNodeExist(){
+			return $this->nodeExist;
+		}
+
+		function setOpenTimes($value){
+			$this->openTimes = $value;
+		}
+
+		function getOpenTimes(){
+			return $this->openTimes;
+		}
+
+		function setProperties($value){
+			array_push($this->properties, $value);
+		}
+
+		function getProperties(){
+			return $this->properties;
+		}
+
+		function getPropertyFromName($name){
+			var $allProperties = this->getProperties();
+			var $resultProperty = null;
+			var $property;
+			foreach($allProperties as $property){
+				if($name == $property->getName()){
+					$resultProperty = $property;
+					break;
+				}
+			}
+
+			return $resultNode
+		}
+	}
+
+	class Property{
+		private $name;
+		private $time;
+		private $correctValue;
+		private $status = array();
+		private $answers = array();
+
+		function setName($value){
+			$this->name = $value;
+		}
+
+		function getName(){
+			return $this->name;
+		}
+
+		function setTime($value){
+			$this->time = $value;
+		}
+
+		function getTime(){
+			return $this->time;
+		}
+
+		function setCorrectValue($value){
+			$this->correctValue = $value;
+		}
+
+		function getCorrectValue(){
+			return $this->correctValue;
+		}
+
+		function setStatus($value){
+			array_push($this->status, $value);
+		}
+
+		function getStatus(){
+			return $this->status;
+		}
+
+		function setAnswers($value){
+			array_push($this->answers, $value);
+		}
+
+		function getAnswers(){
+			return $this->answers;
+		}
+	}
+?>
