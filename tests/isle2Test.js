aa6cc6ba071fe4bb0993147f8079e4e55c237fff
Haoxuan Wu <hw399@cornell.edu>
2014-07-10 17:41:11 -0700

    updated some tests

--- tests/isle2Test.js
+++ tests/isle2Test.js
@@ -8,23 +8,22 @@ var server = new SeleniumServer("C:/Selenium/selenium-server-standalone-2.42.2.j
 
 server.start();
 
-/*var driver = new webdriver.Builder().
-   usingServer(server.address()).
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();*/
-
 var driver = new webdriver.Builder().
     usingServer(server.address()).
-    withCapabilities(webdriver.Capabilities.firefox()).
+    withCapabilities(webdriver.Capabilities.phantomjs()).
     build();
 
 driver.get('http://localhost/LaitsV3/www/login.html');
-driver.findElement(webdriver.By.name('u')).sendKeys('Dragon3');
+driver.findElement(webdriver.By.name('u')).sendKeys('Dragon1');
 driver.findElement(webdriver.By.name('p')).clear();
 driver.findElement(webdriver.By.name('p')).sendKeys('isle2');
-driver.findElement(webdriver.By.name('c')).click();
+driver.findElement(webdriver.By.name('c')).click().then(function() {
+    driver.sleep(1000);
+});
 
-driver.findElement(webdriver.By.id("createNodeButton_label")).click();
+driver.findElement(webdriver.By.id("createNodeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
 
 driver.findElement(webdriver.By.id("selectDescription")).click().then(function() {
     driver.sleep(1000);
@@ -34,6 +33,11 @@ driver.findElement(webdriver.By.id("dijit_MenuItem_2_text")).click().then(functi
 });
 driver.findElement(webdriver.By.id("OkButton_label")).click().then(function() {
     driver.sleep(1000);
+});
+
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.");
 });
 
 driver.findElement(webdriver.By.id("typeId")).click().then(function() {
@@ -46,7 +50,20 @@ driver.findElement(webdriver.By.id("OkButton_label")).click().then(function() {
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("initialValue")).sendKeys("20");
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.");
+});
+
+driver.findElement(webdriver.By.id("initialValue")).sendKeys("20").then(function() {
+    driver.sleep(1000);
+	driver.findElement(webdriver.By.id("selectDescription")).click();
+});
+
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.\nThe value entered for the initial is correct.");
+});
 
 driver.findElement(webdriver.By.id("nodeInputs")).click().then(function() {
     driver.sleep(1000);
@@ -55,13 +72,32 @@ driver.findElement(webdriver.By.id("dijit_MenuItem_12_text")).click().then(funct
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("equationDoneButton")).click();
+driver.findElement(webdriver.By.id("equationDoneButton")).click().then(function() {
+    driver.sleep(1000);
+});
 
-driver.findElement(webdriver.By.id("closeButton_label")).click();
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.\nThe value entered for the initial is correct.\nQuantity 'moose births' not defined yet.\nThe value entered for the equation is correct.");
+});
+
+driver.findElement(webdriver.By.id("closeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+var node = driver.findElement(webdriver.By.id("id4"));
+node.getAttribute('style').then(function(style){
+	var re = /148, 255, 148/;
+	assert(re.test(style));
+});
 
 
 
-driver.findElement(webdriver.By.id("createNodeButton_label")).click();
+
+
+driver.findElement(webdriver.By.id("createNodeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
 
 driver.findElement(webdriver.By.id("selectDescription")).click().then(function() {
     driver.sleep(1000);
@@ -70,11 +106,21 @@ driver.findElement(webdriver.By.id("dijit_MenuItem_17_text")).click().then(funct
     driver.sleep(1000);
 });
 
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.");
+});
+
 driver.findElement(webdriver.By.id("typeId")).click().then(function() {
     driver.sleep(1000);
 });
 driver.findElement(webdriver.By.id("dijit_MenuItem_21_text")).click().then(function() {
     driver.sleep(1000);
+});
+
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.");
 });
 
 driver.findElement(webdriver.By.id("nodeInputs")).click().then(function() {
@@ -91,19 +137,41 @@ driver.findElement(webdriver.By.id("dijit_MenuItem_24_text")).click().then(funct
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("equationDoneButton")).click();
+driver.findElement(webdriver.By.id("equationDoneButton")).click().then(function() {
+    driver.sleep(1000);
+});
 
-driver.findElement(webdriver.By.id("closeButton_label")).click();
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.\nQuantity 'moose birth rate' not defined yet.\nThe value entered for the equation is correct.");
+});
+
+driver.findElement(webdriver.By.id("closeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+var node = driver.findElement(webdriver.By.id("id5"));
+node.getAttribute('style').then(function(style){
+	var re = /148, 255, 148/;
+	assert(re.test(style));
+});
 
 
 
-driver.findElement(webdriver.By.id("createNodeButton_label")).click();
+driver.findElement(webdriver.By.id("createNodeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
 
 driver.findElement(webdriver.By.id("selectDescription")).click().then(function() {
     driver.sleep(1000);
 });
 driver.findElement(webdriver.By.id("dijit_MenuItem_29_text")).click().then(function() {
     driver.sleep(1000);
+});
+
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.");
 });
 
 driver.findElement(webdriver.By.id("typeId")).click().then(function() {
@@ -113,11 +181,34 @@ driver.findElement(webdriver.By.id("dijit_MenuItem_32_text")).click().then(funct
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("initialValue")).sendKeys(".2");
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.");
+});
 
-driver.findElement(webdriver.By.id("selectDescription")).click();
+driver.findElement(webdriver.By.id("initialValue")).sendKeys(".2").then(function() {
+    driver.sleep(1000);
+	driver.findElement(webdriver.By.id("selectDescription")).click();
+});
 
-driver.findElement(webdriver.By.id("closeButton_label")).click();
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.\nThe value entered for the initial is correct.");
+});
+
+driver.findElement(webdriver.By.id("closeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+driver.findElement(webdriver.By.id("OkButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+var node3 = driver.findElement(webdriver.By.id("id6"));
+node3.getAttribute('style').then(function(style) {
+    var re = /148, 255, 148/;
+	assert(re.test(style));
+});
 
 driver.close();
 
--- tests/syntaxErrorTest.js
+++ tests/syntaxErrorTest.js
@@ -9,23 +9,22 @@ var server = new SeleniumServer("C:/Selenium/selenium-server-standalone-2.42.2.j
 
 server.start();
 
-/*var driver = new webdriver.Builder().
-   usingServer(server.address()).
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();*/
-
 var driver = new webdriver.Builder().
     usingServer(server.address()).
     withCapabilities(webdriver.Capabilities.firefox()).
     build();
 
-driver.get('http://localhost/LaitsV3/www/login.html');
-driver.findElement(webdriver.By.name('u')).sendKeys('dragoon2');
-driver.findElement(webdriver.By.name('c')).click();
+driver.get('http://localhost/LaitsV3/www/index.html?u=dragoon7&m=STUDENT&sm=feedback&is=algebraic&p=rabbits&s=login.html&c=Continue').then(function() {
+    driver.sleep(2000);
+});
 
-driver.findElement(webdriver.By.id('createNodeButton_label')).click();
+driver.findElement(webdriver.By.id('createNodeButton_label')).click().then(function() {
+    driver.sleep(1000);
+});
 
-driver.findElement(webdriver.By.id("selectDescription")).click();
+driver.findElement(webdriver.By.id("selectDescription")).click().then(function() {
+    driver.sleep(1000);
+});
 driver.findElement(webdriver.By.id("dijit_MenuItem_2_text")).click().then(function() {
     driver.sleep(1000);
 });
@@ -33,7 +32,9 @@ driver.findElement(webdriver.By.id("OkButton_label")).click().then(function() {
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("typeId")).click();
+driver.findElement(webdriver.By.id("typeId")).click().then(function() {
+    driver.sleep(1000);
+});
 driver.findElement(webdriver.By.id("dijit_MenuItem_10_text")).click().then(function() {
     driver.sleep(1000);
 });
@@ -41,10 +42,16 @@ driver.findElement(webdriver.By.id("OkButton_label")).click().then(function() {
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("initialValue")).sendKeys("24");
+driver.findElement(webdriver.By.id("initialValue")).sendKeys("24").then(function() {
+    driver.sleep(1000);
+});
 
-driver.findElement(webdriver.By.id("selectUnits")).click();
-driver.findElement(webdriver.By.id("dijit_MenuItem_14_text")).click();
+driver.findElement(webdriver.By.id("selectUnits")).click().then(function() {
+    driver.sleep(1000);
+});
+driver.findElement(webdriver.By.id("dijit_MenuItem_14_text")).click().then(function() {
+    driver.sleep(1000);
+});
 
 driver.findElement(webdriver.By.id("equationBox")).sendKeys("ZZZ").then(function() {
     driver.sleep(1000);
@@ -53,15 +60,51 @@ driver.findElement(webdriver.By.id("equationDoneButton_label")).click().then(fun
     driver.sleep(1000);
 });
 
-driver.findElement(webdriver.By.id("equationDoneButton_label")).click();
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "The value entered for the description is correct.\nThe value entered for the type is correct.\nThe value entered for the initial is correct.\nThe value entered for the units is correct.\nUnknown variable 'ZZZ'.");
+});
 
-driver.findElement(webdriver.By.id("closeButton_label")).click();
+driver.findElement(webdriver.By.id("equationDoneButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+driver.findElement(webdriver.By.id("closeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
 
 var node = driver.findElement(webdriver.By.id("id10"));
 node.getAttribute('style').then(function(style) {
       assert.equal(style, "left: 400px; top: 100px; border: 2px dashed green;");
 	  //assert.equal(style, "left: 800px; top: 100px; border: 2px solid green; box-shadow: rgb(0, 0, 0) 0px 0px 5px inset, rgb(0, 0, 0) 0px 0px 10px; background-color: rgb(148, 255, 148);");
 });
+
+driver.findElement(webdriver.By.id("id10")).click();
+
+driver.findElement(webdriver.By.id("equationBox")).sendKeys("net growth").then(function() {
+    driver.sleep(1000);
+});
+
+driver.findElement(webdriver.By.id("equationDoneButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+var messageBox = driver.findElement(webdriver.By.id("messageBox"));
+messageBox.getText().then(function(text){
+	assert.equal(text, "Quantity 'net growth' not defined yet.\nThe value entered for the equation is correct.");
+});
+
+driver.findElement(webdriver.By.id("closeButton_label")).click().then(function() {
+    driver.sleep(1000);
+});
+
+/*var node = driver.findElement(webdriver.By.id("id10"));
+node.getAttribute('style').then(function(style) {
+    var re = /148, 255, 148/;
+	assert(re.test(style));
+});*/
+
+driver.close();
 
 driver.sleep(1000).then(function() {
 	console.log("Passed Syntax Error Test");
