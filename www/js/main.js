571c7e1af9722f4e112bce693d2b19426144ae64
Ritesh Samala <ritesh.samala@gmail.com>
2014-07-26 14:20:16 -0700

    forum buttons included and events attached

--- www/js/main.js
+++ www/js/main.js
@@ -181,6 +181,13 @@ define([
 				registry.byId("nodeeditor").hide();
 			});
 
+            //check if forumurl is present and enable the forum button accordingly
+            if(query.f) {
+                var forumBut = registry.byId("forumButton");
+                var nodeForumBut = registry.byId("nodeForumButton");
+                forumBut.set("disabled", false);
+                nodeForumBut.set("disabled",false);
+            }
 			// Also used in image loading below.
 			var descObj = new description(givenModel);
 			if(query.m == "AUTHOR"){
@@ -272,6 +279,43 @@ define([
 							"height=400, width=600, toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no"
 						   );
 			});
+
+            //For redirecting to the forum from forum button click on header
+            forumBut.on("click",function(){
+                console.log("clicked on main forum button");
+                controllerObject.logging.log('ui-action', {
+                    type: "menu-forum-button",
+                    name: "forum"
+                });
+                var prob_name=givenModel.getTaskName(query.p);
+                console.log("problem name is ", prob_name);
+                // "newwindow": the pop-out window name, not required, could be empty
+                // "height" and "width": pop-out window size
+                // Other properties could be changed as the value of yes or no
+                window.open(query.f+"?&n="+prob_name+"&s="+query.s+"&fid="+query.fid+"&sid="+query.sid,"newwindow",
+                    "height=400, width=600, toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no"
+                );
+            });
+
+            //For redirecting to forum from node editor
+            nodeForumBut.on("click",function(){
+                console.log("clicked on nodeEditor forum button", givenModel);
+                var current_id=controllerObject.currentID;
+                var nid=givenModel.active.getDescriptionID(current_id);
+                var ndesc=givenModel.given.getDescription(nid);
+                controllerObject.logging.log('ui-action', {
+                    type: "menu-forum-button",
+                    name: "forum"
+                });
+                var prob_name=givenModel.getTaskName(query.p);
+                console.log("problem name is ", prob_name);
+                // "newwindow": the pop-out window name, not required, could be empty
+                // "height" and "width": pop-out window size
+                // Other properties could be changed as the value of yes or no
+                window.open(query.f+"?&n="+prob_name+"&s="+query.s+"&fid="+query.fid+"&sid="+query.sid+"&nid="+nid+"&ndesc="+ndesc,"newwindow",
+                    "height=400, width=600, toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no"
+                );
+            });
 		});
 	});
 });
