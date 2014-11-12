4fd8a77b9fc1aae46fc93a372e9c6e85c315c9e3
Pradeep Chaudhari <ppchaudh@asu.edu>
2014-11-12 14:12:28 -0700

    added case for handling deleting prematures nodes too - bug 2470

--- www/js/main.js
+++ www/js/main.js
@@ -193,7 +193,7 @@ define([
 			aspect.after(registry.byId('nodeeditor'), "hide", function(){
 				console.log("Calling session.saveProblem");
 				session.saveProblem(givenModel.model);
-                if(controllerObject.isDescriptionCorrect&&controllerObject.isDescriptionCorrect.status=="incorrect")
+                if(controllerObject.isDescriptionCorrect&& (controllerObject.isDescriptionCorrect.status=="incorrect" || controllerObject.isDescriptionCorrect.status=="premature"))
                     if(controllerObject.currentID == controllerObject.isDescriptionCorrect.id) //to protect deletion of previous correct nodeS
                             drawModel.deleteNode(controllerObject.currentID);
     		});
