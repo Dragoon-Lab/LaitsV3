445649ed4e9caabecab3682e36d2982b193242f2
dAvE <groversachin.211@gmail.com>
2015-02-22 18:54:22 -0700

    score and successfactor added to the code

--- www/js/assessment.js
+++ www/js/assessment.js
@@ -26,73 +26,101 @@ define([
 ], function(declare, array, json, schemaSession){
 	return declare(null, {
 	
-	constructor: function(/* object */ model, /* object */ session){
-		this._model = model;
-		this._session = new schemaSession(session);
-		this.initSchema();
-		this.initSchemaSession();
-	},
+		constructor: function(/* object */ model, /* object */ session){
+			this._model = model;
+			this._session = new schemaSession(session);
+			this.initSchema();
+			this.initSchemaSession();
+		},
 
-	initSchemaSession: function(){
-		array.forEach(this._schemas, function(schema){
-			this._session.logSchema(schema.ID, schema.difficulty);
-		}, this);
-	},
+		initSchemaSession: function(){
+			array.forEach(this._schemas, function(schema){
+				this._session.logSchema(schema.ID, schema.difficulty);
+			}, this);
+		},
 
-	initSchema: function(){
-		this._schemas = this._model.student.getSchemas();
-		array.forEach(this._schemas, function(schema){
-			var resultJSON;
-			this._session.getSchemaApplication(schema.ID).then(function(result){
-				resultJSON = result;
-			});
-			resultJSON = json.parse(resultJSON);
-			if(resultJSON.competence){
-				schema.competence = resultJSON.competence;
+		initSchema: function(){
+			this._schemas = this._model.student.getSchemas();
+			array.forEach(this._schemas, function(schema){
+				var resultJSON;
+				this._session.getSchemaApplication(schema.ID).then(function(result){
+					resultJSON = result;
+				});
+				resultJSON = json.parse(resultJSON);
+				if(resultJSON.competence){
+					schema.competence = resultJSON.competence;
+				}
+			}, this);
+		},
+
+		updateSchema: function(/* object */ time, /* object */ errors){
+			if(time.given == ""){
+				time.given = this._model.student.getDescriptionID(time.node);
 			}
-		}, this);
-	},
 
-	updateSchema: function(/* object */ time, /* object */ errors){
-		if(time.given == ""){
-			time.given = this._model.student.getDescriptionID(time.node);
-		}
+			if(errors.given == ""){
+				errors.given = this._model.student.getDescriptionID(errors.node);
+			}
+			if(errors.given){
+				array.forEach(this._schemas, function(schema){
+					if(schema.nodes.indexOf(errors.given) >= 0){
+						schema.competence.errors += errors.errors;
+						schema.competence.total += errors.total;
+						//schema.competence.timeSpent += error.time
+					}
+				}, this);
+			}
 
-		if(errors.given == ""){
-			errors.given = this._model.student.getDescriptionID(errors.node);
-		}
-		if(errors.given){
+			if(time.given){
+				array.forEach(this._schemas, function(schema){
+					if(schema.nodes.indexOf(time.given) >= 0){
+						schema.competence.timeSpent += time.difference;
+					}
+				}, this);
+			}
+		},
+	
+		saveSchema: function(){
+			this._model.student.setSchemas(this._schemas);
 			array.forEach(this._schemas, function(schema){
-				if(schema.nodes.indexOf(errors.given) >= 0){
-					schema.competence.errors += errors.errors;
-					schema.competence.total += errors.total;
-					//schema.competence.timeSpent += error.time
+				this._session.updateSchemaApplication(schema.ID, schema.competence);
+			}, this);
+		},
+	
+		dummy: function(){
+			array.forEach(this._schemas, function(schema){
+				var competence = 1 - (schema.competence.errors/schema.competence.total);
+				schema.competence.values.dummy = competence;
+			}, this);
+		},
+
+		getScore: function(type){
+			var obj = {};
+			array.forEach(this._schemas, function(schema){
+				obj[schema.ID] = schema.competence.values[type];
+			}, this);
+
+			return obj;
+		},
+
+		getSuccessFactor: function(){
+			var nodes = this._model.given.getNodes();
+			var success = 0;
+			var total = 0;
+			array.forEach(nodes, function(node){
+				var attempts = node.attemptCount;
+				for(var key in attempts){
+					if(attempts[key] == 1 && node.status.hasOwnProperty(key) && node.status[key] == "correct"){
+						success++;
+						total++;
+					} else if(attempts[key] > 0 && node.status.hasOwnProperty(key)){
+						total++;
+					}
 				}
 			}, this);
-		}
 
-		if(time.given){
-			array.forEach(this._schemas, function(schema){
-				if(schema.nodes.indexOf(time.given) >= 0){
-					schema.competence.timeSpent += time.difference;
-				}
-			}, this);
+			return success/total;
 		}
-	},
-	
-	saveSchema: function(){
-		this._model.student.setSchemas(this._schemas);
-		array.forEach(this._schemas, function(schema){
-			this._session.updateSchemaApplication(schema.ID, schema.competence);
-		}, this);
-	},
-	
-	dummy: function(){
-		array.forEach(this._schemas, function(schema){
-			var competence = 1 - (schema.competence.errors/schema.competence.total);
-			schema.competence.values.dummy = competence;
-		}, this);
-	}
 		
-	});	
+	});
 });
