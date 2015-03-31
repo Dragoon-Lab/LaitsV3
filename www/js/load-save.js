78a52738bd5e7644b220524d60377f73a223f609
dAvE <groversachin.211@gmail.com>
2015-03-31 12:34:03 -0700

    code for getting the browser and the version added

--- www/js/load-save.js
+++ www/js/load-save.js
@@ -56,6 +56,25 @@ define([
 		return Number(hash).toString(16);
 	};
 
+	var sayswho = (function(){
+	    var ua= navigator.userAgent, tem, 
+	    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+	    if(/trident/i.test(M[1])){
+	        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+	        return 'IE '+(tem[1] || '');
+	    }
+	    if(M[1]=== 'Chrome'){
+	        tem= ua.match(/\bOPR\/(\d+)/)
+	        if(tem!= null) return 'Opera '+tem[1];
+	    }
+	    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+	    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+	    return {
+			name: M[0],
+			version: M[1]
+		};
+	});
+
 	return declare(null, {
 
 		// The constructor creates a session and sets the sessionId
@@ -77,7 +96,8 @@ define([
 
 			// Create a session
 			this.log("start-session", params);
-
+			this.browser = sayswho();
+			console.log("browser = ", this.browser.name, " version = ", this.browser.version);
 		},
 
 		loadProblem: function(/*object*/ params){
