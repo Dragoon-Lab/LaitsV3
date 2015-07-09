8ca43bc9f8567d81c82f61282e9d710151725530
avaneeshd <avaneeshdesai@gmail.com>
2015-07-09 15:41:30 -0700

    Adding Activity Parameters.js

---
+++ www/js/activity-parameters.js
@@ -0,0 +1,209 @@
+/* global define */
+/**
+ *Dragoon Project
+ *Arizona State University
+ *(c) 2014, Arizona Board of Regents for and on behalf of Arizona State University
+ *
+ *This file is a part of Dragoon
+ *Dragoon is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU Lesser General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *Dragoon is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ *GNU Lesser General Public License for more details.
+ *
+ *You should have received a copy of the GNU Lesser General Public License
+ *along with Dragoon.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+define([
+	"dojo/_base/lang"
+	], function(lang){
+
+	return function() {
+		var activity_params = {
+			construction: {
+				AUTHOR:{
+					allowCreateNode:true,
+					allowGraph:true,
+					allowTable:true,
+					allowForum:true,
+					allowCreateSchema:true,
+					allowProblemTimes:true,
+					allowSaveAs:true,
+					allowMerge:true,
+					allowPreview:true,
+					allowPrettify:true,
+					allowHelp: true,
+
+					allowDeleteNode: true,
+					allowEditNode: true,
+
+					showHints: true,
+					showFeedback: true,
+
+					enable:[
+					],
+
+					allowSliders: true,
+
+					promptSaveAs:true
+				},
+
+				STUDENT:{
+					allowCreateNode:true,
+					allowGraph:true,
+					allowTable:true,
+					allowForum:true,
+					allowPrettify:true,
+					allowHelp: true,
+					allowLessonLearned:true,
+					allowDeleteNode: true,
+					allowEditNode: true,
+
+					showHints: true,
+					showFeedback: true,
+
+					enable:[
+					],
+
+					allowSliders: true,
+					plotAuthorSolution:true
+				},
+
+				COACHED:{
+					allowCreateNode:true,
+					allowGraph:true,
+					allowTable:true,
+					allowForum:true,
+					allowPrettify:true,
+					allowHelp: true,
+					allowLessonLearned:true,
+					allowDeleteNode: true,
+					allowEditNode: true,
+					targetNodeStrategy: true,
+
+					showHints: true,
+					showFeedback: true,
+
+					enable:[
+					],
+
+					allowSliders: true,
+					plotAuthorSolution:true
+				},
+
+				TEST:{
+					allowCreateNode:true,
+					allowGraph:true,
+					allowTable:true,
+					allowForum:true,
+					allowPrettify:true,
+					allowHelp: true,
+
+					allowDeleteNode: true,
+					allowEditNode: true,
+
+					enable:[
+					],
+
+					allowSliders: true,
+					plotAuthorSolution:true
+				},
+
+				EDITOR:{
+					allowCreateNode:true,
+					allowGraph:true,
+					allowTable:true,
+					allowForum:true,
+					allowPrettify:true,
+					allowHelp: true,
+					allowHints:true,
+					allowDeleteNode: true,
+					allowEditNode: true,
+
+					enable:[
+					],
+
+					allowSliders: true
+				}
+			}
+		};
+
+		var config = {
+			_mode: null,
+			_activity: null,
+			_params :{
+				//Menu Options
+				allowCreateNode: false,
+				allowGraph: false,
+				allowTable: false,
+				allowForum: false,
+				allowCreateSchema: false,
+				allowProblemTimes: false,
+				allowSaveAs: false,
+				allowMerge: false,
+				allowPreview: false,
+				allowHints: false,
+				allowPrettify: false,
+				allowHelp: false,
+				allowLessonLearned: false,
+
+				//Node
+				allowDeleteNode: false,
+				allowEditNode: false,
+				targetNodeStrategy:false,
+
+				//Feedback
+				showHints:false,
+				showFeedback:false,
+
+				//Node Editor
+				enable:[
+				],
+
+				autoFill:[
+				],
+
+				//Graph
+				allowSliders:false,
+				plotAuthorSolution:false,
+
+				//Node Click Behaviour
+				showNodeEditor:true,
+
+				//Problem Open Behaviour
+				promptSaveAs:false
+			},
+
+			constructor: function(/*string*/ mode, /*string*/ activity){
+				this._mode = mode;
+				this._activity= activity;
+				this._params = lang.mixin(this._params, activity_params[this._activity][this._mode])
+			},
+
+			getAllParameters: function(){
+				return this._params;
+			},
+
+			get: function(/*string*/ name){
+				return typeof this._params[name] !== "undefined" ? this._params[name] : "undefined";
+			},
+
+			getMode: function(){
+				return this._mode;
+			},
+
+			getActivity: function(){
+				return this._activity;
+			}
+		};
+
+		config.constructor.apply(config, arguments);
+		return config;
+	};
+});
