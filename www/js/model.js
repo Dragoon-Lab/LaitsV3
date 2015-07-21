c570027db0854ff95862bf80aecb5a1ca1a32a1b
Ritesh Reddy Samala <RiteshSamala@riteshs-mbp.mobile.asu.edu>
2015-07-20 21:22:37 -0700

    initialize student model from given model while the activity is incremental

--- www/js/model.js
+++ www/js/model.js
@@ -279,8 +279,81 @@ define([
 
 				this.isCompleteFlag = this.matchesGivenSolution();
 			},
+            initializeStudentModel: function(/*Object*/ solutionGraph){
+                this.model = solutionGraph;
+                var thisModel = this;
+                //empty the student model
+                this.model.task.studentModelNodes = [];
+                array.forEach(this.student.getNodes(),function(nodeIds){
+                    console.log("tmz",nodeIds);
+                });
+                var largest = 0;
+                var intID = function(/*object*/ node){
+                    if(node.ID.length >= 2 && node.ID.slice(0, 2) == "id"){
+                        var n = parseInt(node.ID.slice(2));
+                        if(n && n > largest)
+                            largest = n;
+                    }
+                };
+                array.forEach(this.given.getNodes(), intID);
+                this._ID=largest;
+                var nodeStore= [];
 
-			getModelAsString: function(){
+                array.forEach(this.given.getNodes(),function(currentNode){
+                    var newNode = thisModel.student.addNodeInc();
+                    nodeStore[currentNode.ID] = newNode;
+                });
+                array.forEach(this.given.getNodes(),function(currentNode){
+                    var newNodeID = nodeStore[currentNode.ID];
+                    thisModel.student.setDescriptionID(newNodeID, currentNode.ID);
+                    thisModel.student.setInitial(newNodeID, currentNode.initial);
+                    thisModel.student.setUnits(newNodeID, currentNode.units);
+                    thisModel.student.setType(newNodeID, currentNode.type);
+                    thisModel.student.setTweakDirection(newNodeID,currentNode.tweakDirection);
+                    if(currentNode.equation){
+                        var inputs = [];
+                        var isExpressionValid = true;
+                        var equation = currentNode.equation;
+                        array.forEach(currentNode.inputs, lang.hitch(this, function(input){
+                            var studentNodeID = nodeStore[input.ID];
+                            if(studentNodeID){
+                                inputs.push({ "ID": studentNodeID});
+                                var regexp = "(" +input.ID +")([^0-9]?)";
+                                var re = new RegExp(regexp);
+                                equation = equation.replace(re, studentNodeID+"$2");
+                            }
+                            else{
+                                isExpressionValid = false;
+                            }
+                        }));
+                        if(isExpressionValid){
+                            thisModel.student.setInputs(inputs, newNodeID);
+                            thisModel.student.setEquation(newNodeID, equation);
+                            thisModel.student.setStatus(newNodeID, "equation" , {"disabled":true,"status":"correct"});
+                        }
+                        else{
+                            thisModel.student.setInputs([], newNodeID);
+                            thisModel.student.setEquation(newNodeID, "");
+                            thisModel.student.setStatus(newNodeID, "equation" , {"disabled":false,"status":"incorrect"});
+                        }
+                    }
+                    thisModel.student.setPosition(newNodeID, currentNode.position);
+
+                    //Set default status to correct for all the fields
+                    thisModel.student.setStatus(newNodeID, "description" , {"disabled":true,"status":"correct"});
+                    thisModel.student.setStatus(newNodeID, "type" , {"disabled":true,"status":"correct"});
+                    if(typeof currentNode.units !== "undefined"){
+                        thisModel.student.setStatus(newNodeID, "units" , {"disabled":true,"status":"correct"});
+                    }
+                    if(currentNode.type === "parameter" || currentNode.type === "accumulator" ){
+                        thisModel.student.setStatus(newNodeID, "initial" , {"disabled":true,"status":"correct"});
+                    }
+                });
+                console.log("final Model is",thisModel);
+            },
+
+
+            getModelAsString: function(){
 				// Summary: Returns a JSON object in string format
 				//			Should only be used for debugging.
 				return JSON.stringify(this.model, null, 4);
@@ -578,15 +651,8 @@ define([
 					}
 				}
 				nodes.splice(index, 1);
-			},
-			initializeStudentModel: function(/*Object*/ solutionGraph){
-				this.model = solutionGraph;
-				console.log("inside the initialize function");
-				//array.forEach(this.given.getNodes(),function(modelNode){
-				  //  console.log("model node is",modelNode);
-				//});
 			}
-		};
+        };
 
 		obj.given = lang.mixin({
 			addNode: function(options){
@@ -610,7 +676,7 @@ define([
 				obj.model.task.givenModelNodes.push(newNode);
 				return newNode.ID;
 			},
-			createSchema: function(/* string */ schemaID){
+            createSchema: function(/* string */ schemaID){
 				var newSchema = {
 					ID: schemaID||"schema"+obj._SID++,
 					schemaClass: "",
@@ -989,7 +1055,25 @@ define([
 				obj.model.task.studentModelNodes.push(newNode);
 				return newNode.ID;
 			},
-			addSchemaTime: function(/* string */ id, /* number */ value){
+
+            addNodeInc: function(options){
+                // Summary: builds a new node in the student model and
+                //			returns the node's ID.	Can optionally set
+                //			properties.
+                obj._updateNextXYPosition();
+                var newNode = lang.mixin({
+                    ID: "id" + obj._ID++,
+                    inputs: [],
+                    position: {x: obj.x, y: obj.y},
+                    status: {},
+                    tweakDirection : ''
+                }, options || {});
+                obj.model.task.studentModelNodes.push(newNode);
+                return newNode.ID;
+            },
+
+
+            addSchemaTime: function(/* string */ id, /* number */ value){
 				var givenID = this.getDescriptionID(id);
 
 				if(givenID){
