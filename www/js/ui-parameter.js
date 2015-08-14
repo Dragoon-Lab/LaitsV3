0f80509677852c6300270a2a88b6c36ba198f88f
avaneeshd <avaneeshdesai@gmail.com>
2015-08-14 00:06:59 -0700

    changed indentation for ui-parameters.js

--- www/js/ui-parameter.js
+++ www/js/ui-parameter.js
@@ -12,241 +12,240 @@
  *
  *Dragoon is distributed in the hope that it will be useful,
  *but WITHOUT ANY WARRANTY; without even the implied warranty of
- *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
  *GNU Lesser General Public License for more details.
  *
  *You should have received a copy of the GNU Lesser General Public License
  *along with Dragoon.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
- // this configuration describe parameters that define UI parameters of dragoon
- define(["dojo/_base/lang"], function(lang){
-	 return function() {
-		 // specific parameters that apply for modes and activity grid  
-		 var parameters = {
-			 construction : [
+	// this configuration describe parameters that define UI parameters of dragoon
+define(["dojo/_base/lang"], function (lang) {
+	return function () {
+		// specific parameters that apply for modes and activity grid
+		var parameters = {
+			construction: [
 				{
-					"mode" : ["AUTHOR", "STUDENT", "COACHED" , "TEST"] ,
-					"param" : { 
-						"showColor" : true,
-						"nodeBorderSize" : "3px ",
-						"createNodeButton" : "none",
-						"graphButton" : "none",
-						"tableButton" : "none",
-						"forumButton" : "none",
-						"schemaButton" : "none",
-						"descButton" : "none",
-						"saveButton" : "none",
-						"mergeButton" : "none",
-						"previewButton" : "none",
-						"slidesButton" : "none",
-						"lessonsLearnedButton" : "none",
-						"doneButton" : "none",
+					"mode": ["AUTHOR", "STUDENT", "COACHED", "TEST"],
+					"param": {
+						"showColor": true,
+						"nodeBorderSize": "3px ",
+						"createNodeButton": "none",
+						"graphButton": "none",
+						"tableButton": "none",
+						"forumButton": "none",
+						"schemaButton": "none",
+						"descButton": "none",
+						"saveButton": "none",
+						"mergeButton": "none",
+						"previewButton": "none",
+						"slidesButton": "none",
+						"lessonsLearnedButton": "none",
+						"doneButton": "none",
 						"prettifyButton": "none",
-						"nodeBorderCompleteColor" : "gray",
-						"nodeBorderCompleteStyle" : "solid ",
-						"nodeBorderInCompleteStyle" : "dashed ",
-						"nodeBorderSuccessColor" : "green",
-						"nodeBorderFeedbackColor" : "yellow",
-						"nodeBorderFailColor" : "red",
-						
-					}
-				},
-				{ 	"mode" : ["AUTHOR"] ,
-					"param" : {
-						"createNodeButton" : "inline",
-						"graphButton" : "inline",
-						"tableButton" : "inline",
-						"forumButton" : "inline",
-						"schemaButton" : "inline",
-						"descButton" : "inline",
-						"saveButton" : "inline",
-						"mergeButton" : "inline",
-						"previewButton" : "inline",
-						"slidesButton" : "inline",
-						"lessonsLearnedButton" : "inline",
-						"doneButton" : "inline",
-						"prettifyButton": "inline"
-						
+						"nodeBorderCompleteColor": "gray",
+						"nodeBorderCompleteStyle": "solid ",
+						"nodeBorderInCompleteStyle": "dashed ",
+						"nodeBorderSuccessColor": "green",
+						"nodeBorderFeedbackColor": "yellow",
+						"nodeBorderFailColor": "red",
+
 					}
 				},
 				{
-					"mode" : ["STUDENT", "COACHED" , "TEST"] ,
-					"param" : {
-						"createNodeButton" : "inline",
-						"graphButton" : "inline",
-						"tableButton" : "inline",
-						"forumButton" : "inline",
-						"slidesButton" : "inline",
+					"mode": ["AUTHOR"],
+					"param": {
+						"createNodeButton": "inline",
+						"graphButton": "inline",
+						"tableButton": "inline",
+						"forumButton": "inline",
+						"schemaButton": "inline",
+						"descButton": "inline",
+						"saveButton": "inline",
+						"mergeButton": "inline",
+						"previewButton": "inline",
+						"slidesButton": "inline",
+						"lessonsLearnedButton": "inline",
+						"doneButton": "inline",
+						"prettifyButton": "inline"
+
+					}
+				},
+				{
+					"mode": ["STUDENT", "COACHED", "TEST"],
+					"param": {
+						"createNodeButton": "inline",
+						"graphButton": "inline",
+						"tableButton": "inline",
+						"forumButton": "inline",
+						"slidesButton": "inline",
 						"prettifyButton": "inline",
 						"doneButton": "inline",
-						"lessonsLearnedButton" : "inline"
+						"lessonsLearnedButton": "inline"
 					}
 				},
 				{
-					"mode" : ["TEST", "EDITOR"] ,
-					"param" : {
-						"showColor" : false
+					"mode": ["TEST", "EDITOR"],
+					"param": {
+						"showColor": false
 					}
 				}
-				
-			 ],
-			 incremental : [
-			 	{
-					"mode" : ["STUDENT", "COACHED"] ,
-					"param" : {
-						"showColor" : true,
-						"nodeBorderSize" : "3px ",
-						"createNodeButton" : "none",
-						"graphButton" : "none",
-						"tableButton" : "none",
-						"forumButton" : "none",
-						"schemaButton" : "none",
-						"descButton" : "none",
-						"saveButton" : "none",
-						"mergeButton" : "none",
-						"previewButton" : "none",
-						"slidesButton" : "none",
-						"lessonsLearnedButton" : "none",
-						"doneButton" : "inline",
+
+			],
+			incremental: [
+				{
+					"mode": ["STUDENT", "COACHED"],
+					"param": {
+						"showColor": true,
+						"nodeBorderSize": "3px ",
+						"createNodeButton": "none",
+						"graphButton": "none",
+						"tableButton": "none",
+						"forumButton": "none",
+						"schemaButton": "none",
+						"descButton": "none",
+						"saveButton": "none",
+						"mergeButton": "none",
+						"previewButton": "none",
+						"slidesButton": "none",
+						"lessonsLearnedButton": "none",
+						"doneButton": "inline",
 						"prettifyButton": "inline",
-						"nodeBorderCompleteColor" : "gray",
-						"nodeBorderCompleteStyle" : "solid ",
-						"nodeBorderInCompleteStyle" : "dashed ",
-						"nodeBorderSuccessColor" : "green",
-						"nodeBorderFeedbackColor" : "yellow",
-						"nodeBorderFailColor" : "red",
+						"nodeBorderCompleteColor": "gray",
+						"nodeBorderCompleteStyle": "solid ",
+						"nodeBorderInCompleteStyle": "dashed ",
+						"nodeBorderSuccessColor": "green",
+						"nodeBorderFeedbackColor": "yellow",
+						"nodeBorderFailColor": "red",
 						"qualitativeChangeButtons": "block"
-						
+
 					}
 				},
-				 {
-					 "mode": ["TEST","EDITOR"],
-					 "param" : {
-						 "showColor" : false,
-						 "nodeBorderSize" : "3px ",
-						 "createNodeButton" : "none",
-						 "graphButton" : "none",
-						 "tableButton" : "none",
-						 "forumButton" : "none",
-						 "schemaButton" : "none",
-						 "descButton" : "none",
-						 "saveButton" : "none",
-						 "mergeButton" : "none",
-						 "previewButton" : "none",
-						 "slidesButton" : "none",
-						 "lessonsLearnedButton" : "none",
-						 "doneButton" : "inline",
-						 "prettifyButton": "inline",
-						 "nodeBorderCompleteColor" : "gray",
-						 "nodeBorderCompleteStyle" : "solid ",
-						 "nodeBorderInCompleteStyle" : "dashed ",
-						 "nodeBorderSuccessColor" : "gray",
-						 "nodeBorderFeedbackColor" : "gray",
-						 "nodeBorderFailColor" : "gray",
-						 "qualitativeChangeButtons": "block"
-					 }
-				 }],
+				{
+					"mode": ["TEST", "EDITOR"],
+					"param": {
+						"showColor": false,
+						"nodeBorderSize": "3px ",
+						"createNodeButton": "none",
+						"graphButton": "none",
+						"tableButton": "none",
+						"forumButton": "none",
+						"schemaButton": "none",
+						"descButton": "none",
+						"saveButton": "none",
+						"mergeButton": "none",
+						"previewButton": "none",
+						"slidesButton": "none",
+						"lessonsLearnedButton": "none",
+						"doneButton": "inline",
+						"prettifyButton": "inline",
+						"nodeBorderCompleteColor": "gray",
+						"nodeBorderCompleteStyle": "solid ",
+						"nodeBorderInCompleteStyle": "dashed ",
+						"nodeBorderSuccessColor": "gray",
+						"nodeBorderFeedbackColor": "gray",
+						"nodeBorderFailColor": "gray",
+						"qualitativeChangeButtons": "block"
+					}
+				}],
 
-			 incrementalDemo : [{
-					 "mode" : ["STUDENT", "COACHED"] ,
-					 "param" : {
-						 "showColor" : true,
-						 "nodeBorderSize" : "3px ",
-						 "createNodeButton" : "none",
-						 "graphButton" : "none",
-						 "tableButton" : "none",
-						 "forumButton" : "none",
-						 "schemaButton" : "none",
-						 "descButton" : "none",
-						 "saveButton" : "none",
-						 "mergeButton" : "none",
-						 "previewButton" : "none",
-						 "slidesButton" : "none",
-						 "lessonsLearnedButton" : "none",
-						 "doneButton" : "inline",
-						 "prettifyButton": "inline",
-						 "nodeBorderCompleteColor" : "gray",
-						 "nodeBorderCompleteStyle" : "solid ",
-						 "nodeBorderInCompleteStyle" : "dashed ",
-						 "nodeBorderSuccessColor" : "green",
-						 "nodeBorderFeedbackColor" : "yellow",
-						 "nodeBorderFailColor" : "red",
-						 "qualitativeChangeButtons": "none"
+			incrementalDemo: [{
+				"mode": ["STUDENT", "COACHED"],
+				"param": {
+					"showColor": true,
+					"nodeBorderSize": "3px ",
+					"createNodeButton": "none",
+					"graphButton": "none",
+					"tableButton": "none",
+					"forumButton": "none",
+					"schemaButton": "none",
+					"descButton": "none",
+					"saveButton": "none",
+					"mergeButton": "none",
+					"previewButton": "none",
+					"slidesButton": "none",
+					"lessonsLearnedButton": "none",
+					"doneButton": "inline",
+					"prettifyButton": "inline",
+					"nodeBorderCompleteColor": "gray",
+					"nodeBorderCompleteStyle": "solid ",
+					"nodeBorderInCompleteStyle": "dashed ",
+					"nodeBorderSuccessColor": "green",
+					"nodeBorderFeedbackColor": "yellow",
+					"nodeBorderFailColor": "red",
+					"qualitativeChangeButtons": "none"
 
-					 }
-				 },
-				 {
-					 "mode": ["TEST","EDITOR"],
-					 "param" : {
-						 "showColor" : false,
-						 "nodeBorderSize" : "3px ",
-						 "createNodeButton" : "none",
-						 "graphButton" : "none",
-						 "tableButton" : "none",
-						 "forumButton" : "none",
-						 "schemaButton" : "none",
-						 "descButton" : "none",
-						 "saveButton" : "none",
-						 "mergeButton" : "none",
-						 "previewButton" : "none",
-						 "slidesButton" : "none",
-						 "lessonsLearnedButton" : "none",
-						 "doneButton" : "inline",
-						 "prettifyButton": "inline",
-						 "nodeBorderCompleteColor" : "gray",
-						 "nodeBorderCompleteStyle" : "solid ",
-						 "nodeBorderInCompleteStyle" : "dashed ",
-						 "nodeBorderSuccessColor" : "gray",
-						 "nodeBorderFeedbackColor" : "gray",
-						 "nodeBorderFailColor" : "gray",
-						 "qualitativeChangeButtons": "none"
+				}
+			},
+				{
+					"mode": ["TEST", "EDITOR"],
+					"param": {
+						"showColor": false,
+						"nodeBorderSize": "3px ",
+						"createNodeButton": "none",
+						"graphButton": "none",
+						"tableButton": "none",
+						"forumButton": "none",
+						"schemaButton": "none",
+						"descButton": "none",
+						"saveButton": "none",
+						"mergeButton": "none",
+						"previewButton": "none",
+						"slidesButton": "none",
+						"lessonsLearnedButton": "none",
+						"doneButton": "inline",
+						"prettifyButton": "inline",
+						"nodeBorderCompleteColor": "gray",
+						"nodeBorderCompleteStyle": "solid ",
+						"nodeBorderInCompleteStyle": "dashed ",
+						"nodeBorderSuccessColor": "gray",
+						"nodeBorderFeedbackColor": "gray",
+						"nodeBorderFailColor": "gray",
+						"qualitativeChangeButtons": "none"
 
-					 }
-				 }],
-			 execution : [],
-			 "automated_execution" : [],
-			 	 
-		 };
+					}
+				}],
+			execution: [],
+			"automated_execution": [],
 
-		 var config = {
-			 _mode : null,
-			 _activity : null,
-			 _params : {
-				 	
-			 },
-			 constructor : function(/* string */ mode, /* string */ activity){
+		};
+
+		var config = {
+			_mode: null,
+			_activity: null,
+			_params: {},
+			constructor: function (/* string */ mode, /* string */ activity) {
 				this._mode = mode;
 				this._activity = activity;
-				 // iterate through all object in the activity and merge all params for given mode:
+				// iterate through all object in the activity and merge all params for given mode:
 				//console.log("In constructor", this._mode, this._activity);
-				for(var idx in parameters[this._activity]){
+				for (var idx in parameters[this._activity]) {
 					var obj = parameters[this._activity][idx];
-					if(obj.mode.indexOf(this._mode) > -1) this._params = lang.mixin(this._params , obj.param);	
-				}	 
-			 },
-			 
-			 //operations available throught the config api
-			 get : function(/*string*/ name){
+					if (obj.mode.indexOf(this._mode) > -1) this._params = lang.mixin(this._params, obj.param);
+				}
+			},
+
+			//operations available throught the config api
+			get: function (/*string*/ name) {
 				var result = null;
-				
-				(typeof this._params[name] != "undefined") ? result =  this._params[name] : result = undefined;
-			 	return result;
-			 },
-			 getMode : function(){
-				 return this._mode;
-			 },
-			 getActivity : function(){
-				 return this._activity;
-			 },
-			 //returns object containing all(general and specific) config parameters for the given mode and activity
-			 getAllParameters : function(){
-				 return this._params;
-			 }
-		 };
-		
-		 config.constructor.apply(config, arguments);
-		
-		 return config;
-	 }
- });
+
+				(typeof this._params[name] != "undefined") ? result = this._params[name] : result = undefined;
+				return result;
+			},
+			getMode: function () {
+				return this._mode;
+			},
+			getActivity: function () {
+				return this._activity;
+			},
+			//returns object containing all(general and specific) config parameters for the given mode and activity
+			getAllParameters: function () {
+				return this._params;
+			}
+		};
+
+		config.constructor.apply(config, arguments);
+
+		return config;
+	}
+});
