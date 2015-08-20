03b94b44917bfa53e9be92c0e64c2d32ef308bf4
Jon Wetzel <jon.wetzel@asu.edu>
2015-08-20 15:44:09 -0700

    Improved windows test runner.

--- tests/RunTests.bat
+++ tests/RunTests.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting selenium server...
 START /B java -jar .\selenium-server-standalone-2.45.0.jar -log selenium.log
-timeout 5
+timeout 10
 echo Engage!
 rem call mocha ./scripts/coreTests/functionTest.js -t 30000
 call mocha %1 -t 30000
