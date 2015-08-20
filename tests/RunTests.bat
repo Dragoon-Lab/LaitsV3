85409b081296cbc36d23630cb2170e41acdd039f
Jon Wetzel <jon.wetzel@asu.edu>
2015-08-20 12:02:42 -0700

    Fixed dtestlib.js so tests run again.  Added windows batch file.

---
+++ tests/RunTests.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting selenium server...
+START /B java -jar .\selenium-server-standalone-2.45.0.jar -log selenium.log
+timeout 5
+echo Engage!
+rem call mocha ./scripts/coreTests/functionTest.js -t 30000
+call mocha %1 -t 30000
+
+echo Shutting down selenium server...
+curl http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
