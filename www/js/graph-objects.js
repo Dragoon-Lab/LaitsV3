ef4086c24f36dc41f7017b907b6a3efcd7f08a80
avaneeshd <avaneeshdesai@gmail.com>
2015-09-16 14:44:34 -0700

    Add line break after operator in accumulator node

--- www/js/graph-objects.js
+++ www/js/graph-objects.js
@@ -55,7 +55,8 @@ define([
 						parse = expression.parse(parse);
 						// May want to change symbols to "sum" and "product"
 						parameter = expression.isSum(parse) && expression.isProduct(parse) ? '' : expression.isSum(parse) ? '+' : expression.isProduct(parse) ? '*' : '';
-						parameter = '<strong style="font-size:18px">' + parameter + '</strong>';
+						if(parameter)
+							parameter = '<strong style="font-size:18px">' + parameter + '</strong><br/>';
 					} catch (err) {
 						console.log("Parse Error" + err);
 					}
@@ -66,7 +67,11 @@ define([
 				if (!unitsValue) {
 					unitsValue = '';
 				}
-				initialValue += "<br/>" + unitsValue;
+				if(initialValue) {
+					initialValue += "<br/>" + unitsValue;
+				}else{
+					initialValue = unitsValue;
+				}
 				if(type === 'triangle') initialValue = 'Click here!';
 				htmlContent = '<div id=' + nodeId + 'Label  class="bubble"><div class="' + type + 'Wrapper"><strong class="nodeContent">' + parameter + initialValue + '</strong></div><div class=' + type + 'Div><strong>' + nodeName + '</strong></div></div>';
 				console.log(nodeName);
