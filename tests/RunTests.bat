0e9c38fe18b5f498fbb1c69f3268c0587bb9d7b6
Jon Wetzel <jon.wetzel@asu.edu>
2015-10-02 14:54:39 -0700

    Added full test call to RunTests.bat

--- tests/RunTests.bat
+++ tests/RunTests.bat
@@ -3,8 +3,13 @@ echo Starting selenium server...
 START /B java -jar .\selenium-server-standalone-2.45.0.jar -log selenium.log
 timeout 10
 echo Engage!
-call mocha ./scripts/coreTests/functionTest.js -t 30000
+
+rem USE NEXT LINE TO TEST MOCHA ITSELF:
 rem call mocha %1 -t 30000
+
+rem USE NEXT TWO LINES TO RUN NORMAL TESTS:
+call mocha ./scripts/coreTests/ -t 30000
+call mocha ./scripts/bugTests/ -t 30000
 
 echo Shutting down selenium server...
 curl http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
