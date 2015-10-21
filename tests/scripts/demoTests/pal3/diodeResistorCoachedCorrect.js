f174ba47e371941717bac194ff656388ac2c7ce9
jonwetzel <jon.wetzel@asu.edu>
2015-10-21 15:28:34 -0700

    Add complete model a diode test and support for new URL params.

--- tests/scripts/demoTests/pal3/diodeResistorCoachedCorrect.js
+++ tests/scripts/demoTests/pal3/diodeResistorCoachedCorrect.js
@@ -25,12 +25,14 @@ var async = sync.asyncIt;
 describe("Coached mode with correct diode resistor 2", function() {
 
     before(async(function (done) {
-            dtest.openProblem(client,[["problem","diode-resistor"],["mode","COACHED"],
-                                      ["section","PAL3-test"],
-                                      ["logging","true"]]);
+        var date = new Date();
+        dtest.openProblem(client,[["problem","diode-resistor"],["mode","COACHED"],
+                                  ["section","PAL3-test"],["revisionID","jon.wetzel@asu.edu"+date.getTime()],
+                                  ["user","jon.wetzel@asu.edu"],["restart","on"],
+                                  ["logging","true"],["topic","Diode Action"]]);
     }));
 
-     describe("Creating nodes:", function(){
+    describe("Creating nodes:", function(){
         it("Should create Function node - I around loop", async(function(){
             dtest.openEditorForNode(client, "I around loop");
             dtest.setNodeType(client, "Function");
@@ -58,24 +60,40 @@ describe("Coached mode with correct diode resistor 2", function() {
             dtest.setNodeInitialValue(client, 500);
             dtest.setNodeUnits(client, "ohms");
             dtest.nodeEditorDone(client);
-            dtest.popupWindowPressOk(client);
         }));
 
-        it("Should fill in parameter node - V across D1", async(function(){
+        it("Should fill in function node - V across D1", async(function(){
             dtest.openEditorForNode(client, "V across D1");
             dtest.setNodeType(client, "Function");
             dtest.setNodeUnits(client, "volts");
-            dtest.setNodeExpression(client, "Source V-V across D1");
+            dtest.setNodeExpression(client, "if(V across D1 when open>Knee voltage,Knee voltage,V across D1 when open)");
             dtest.nodeEditorDone(client);
-            dtest.popupWindowPressOk(client);
         }));
+
+        it("Should fill in function node - V across D1 when open", async(function(){
+            dtest.openEditorForNode(client, "V across D1 when open");
+            dtest.setNodeType(client, "Function");
+            dtest.setNodeUnits(client, "volts");
+            dtest.setNodeExpression(client, "Source V");
+            dtest.nodeEditorDone(client);
+        }));
+
+        it("Should fill in parameter node - Knee voltage", async(function(){
+            dtest.openEditorForNode(client, "Knee voltage");
+            dtest.setNodeType(client, "Parameter");
+            dtest.setNodeInitialValue(client, 0.7);
+            dtest.setNodeUnits(client, "volts");
+            dtest.nodeEditorDone(client);
+            //dtest.popupWindowPressOk(client);
+        }));        
 
     });
 
     describe("Checking node colors", function(){
         it("Nodes should have correct border and fill colors", async(function(){
             //Defines which nodes to check
-            var nodesToCheck = ["current", "voltage at VL", "Resistance"];
+            var nodesToCheck = ["I around loop", "V across R1", "R or R1", 
+                                "V across D1", "V across D1 when open", "Knee voltage"];
             //Does test for all nodes
             nodesToCheck.forEach(function(element){
                 //Gets values
@@ -93,63 +111,6 @@ describe("Coached mode with correct diode resistor 2", function() {
         }));
     });
 
-    describe("Checking Nodes:", function(){
-        
-        afterEach(async(function(){
-            dtest.nodeEditorDone(client);
-        }));
-
-        it("Should have correct Function values and colors", async(function(){
-            var nodeName = "current"
-            dtest.openEditorForNode(client, nodeName);
-
-            atest.checkNodeValues([["nodeName", "current"],
-                                    ["expectedDescription", "Current through resistor, diode and battery"],
-                                    ["expectedNodeType", "Function"],
-                                    ["expectedNodeUnits", "amps"],
-                                    ["expectedExpression", "voltage at VL/Resistance"],
-                                    ["expectedDescriptionColor", "green"],
-                                    ["expectedTypeColor", "green"],
-                                    ["expectedInitialColor", "gray"],
-                                    ["expectedUnitsColor", "green"],
-                                    ["expectedExpressionColor", "green"]], dtest, client);
-        }));
-
-        it("Should have correct function values and colors", async(function(){
-            var nodeName = "voltage at VL"
-
-            dtest.openEditorForNode(client, nodeName);
-
-            atest.checkNodeValues([["nodeName", "voltage at VL"],
-                                    ["expectedDescription", "Voltage at node VL"],
-                                    ["expectedNodeType", "Function"],
-                                    ["expectedNodeUnits", "volts"],
-                                    ["expectedExpression", "max(0,voltage source)"],
-                                    ["expectedDescriptionColor", "green"],
-                                    ["expectedTypeColor", "green"],
-                                    ["expectedInitialColor", "gray"],
-                                    ["expectedUnitsColor", "green"],
-                                    ["expectedExpressionColor", "green"]], dtest, client);
-        }));
-
-        it("Should have correct parameter values and colors", async(function(){
-            var nodeName = "Resistance";
-
-            dtest.openEditorForNode(client, nodeName);
-
-            atest.checkNodeValues([["nodeName", "Resistance"],
-                                    ["expectedDescription", "Resistance of the resistor"],
-                                    ["expectedNodeType", "Parameter"],
-                                    ["expectedInitialValue", "500"],
-                                    ["expectedNodeUnits", "ohms"],
-                                    ["expectedDescriptionColor", "green"],
-                                    ["expectedTypeColor", "green"],
-                                    ["expectedInitialColor", "green"],
-                                    ["expectedUnitsColor", "green"],
-                                    ["expectedExpressionColor", "gray"]], dtest, client);            
-        }));
-    });
-
     describe("Checking graph/table window:", function(){
         it("Should open the table window and check the table values", async(function(){
             dtest.menuOpenTable(client);
@@ -157,16 +118,16 @@ describe("Coached mode with correct diode resistor 2", function() {
             var time = 0;
             var message = "";
             if(!(dtest.tableGetValue(client, 0, 3) == "0.00")){
-                message += "\n First - required: 0.00, found: "+dtest.tableGetValue(client, 0, 0);
+                message += "\n First - required: 0.00, found: "+dtest.tableGetValue(client, 0, 3);
                 currentValue = false;
             }
-            if(!(dtest.tableGetValue(client, 199 , 3) == "0.00")){
-                message += "\n Last - required: 0.00, found: "+dtest.tableGetValue(client, 199, 0);
+            if(!(dtest.tableGetValue(client, 199 , 3) == "-0.314")){
+                message += "\n Last - required: -0.314, found: "+dtest.tableGetValue(client, 199, 3);
                 currentValue = false;
             }
 
             assert(currentValue === true,
-                "Values in the \"current (amps)\" column were incorrect. " + message);
+                "Values in the \"voltage across D1 (volts)\" column were incorrect. " + message);
         }));
     });
     after(async(function(done){
