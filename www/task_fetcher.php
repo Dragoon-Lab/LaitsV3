cb263e45efd23cc915510b0f7a3358708c72b1aa
Ritesh Reddy Samala <RiteshSamala@Riteshs-MacBook-Pro.local>
2016-03-10 15:31:45 -0600

    possible fix for go lab integration

--- www/task_fetcher.php
+++ www/task_fetcher.php
@@ -148,6 +148,17 @@ EOT;
 	}
    $result = $mysqli->query($query)
      or trigger_error("Previous work query failed." . $mysqli->error);
+
+      //Ritesh: this is piece of code to retrieve query from a copy parameter , if it is set and existing folder does not contain the specific problem
+      if($result->num_rows == 0 && $_GET['cp']){
+          $new_group = $_GET['cp'];
+          $query = <<<EOT
+         SELECT t1.solution_graph, t1.share FROM solutions AS t1 JOIN session AS t2 USING (session_id)
+              WHERE t2.section = '$section' AND t2.mode = '$mode'
+              AND t2.problem = '$shortProblemName' AND t2.group = '$new_group' AND t2.activity = '$activity' ORDER BY t1.time DESC LIMIT 1
+EOT;
+      $result = $mysqli->query($query);
+      }
    if($row = $result->fetch_row()){
        printModel($row);
     mysqli_close($mysqli);
