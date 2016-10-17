da623381afd5d890dbefb9ea9f92f8040025fc6c
Ritesh Reddy Samala <RiteshSamala@Riteshs-MacBook-Pro.local>
2016-10-17 10:10:55 -0700

    API functions for renaming models and folders

--- www/database.php
+++ www/database.php
@@ -51,116 +51,142 @@
 			return json_encode($problems);
 		}
 
-        function getNonClassProblems($parameters){
-            $group = $this->db_connection->real_escape_string($parameters['g']);
-            //we made sure group is unique each time
-            //query by group name and return problem names
-            $query = "select DISTINCT problem from session where `group` = '$group'";
-            $result = $this->getDBResults($query);
-            $nc_probs = array();
-            if($result->num_rows != 0){
-                while($row = $result->fetch_assoc()){
-                    $nc_probs[$row['problem']] = $row['problem'];
-                }
-            } else {
-                return null;
-            }
+		function getNonClassProblems($parameters){
+			$group = $this->db_connection->real_escape_string($parameters['g']);
+			//we made sure group is unique each time
+			//query by group name and return problem names
+			$query = "select DISTINCT problem from session where `group` = '$group'";
+			$result = $this->getDBResults($query);
+			$nc_probs = array();
+			if($result->num_rows != 0){
+				while($row = $result->fetch_assoc()){
+					$nc_probs[$row['problem']] = $row['problem'];
+				}
+			} else {
+				return null;
+			}
 
-            return json_encode($nc_probs);
+			return json_encode($nc_probs);
 
-        }
+		}
 
-        function deleteNonClassProblems($parameters){
-            //first delete folders which implies updating the group ids with tag deleted
-            $del_folders = $parameters['df'];
-            $del_models = $parameters['dm'];
-            $del_suc1=$del_suc2=false;
+		function deleteNonClassProblems($parameters){
+			//first delete folders which implies updating the group ids with tag deleted
+			$del_folders = $parameters['df'];
+			$del_models = $parameters['dm'];
+			$del_suc1=$del_suc2=false;
 
-            foreach($del_folders as $eachFolder){
-                $user = explode("-",$eachFolder);
-                $new_folder = ($user[1] == 'private') ? $user[0] : $user[1];
-                $new_folder = $new_folder."-deleted";
-                //escape quotes, make strings query safe
-                $new_folder = $this->db_connection->real_escape_string($new_folder);
-                $eachFolder = $this->db_connection->real_escape_string($eachFolder);
-                $del_folder_query = "update session set `group` = '$new_folder',time = time where `group` = '$eachFolder' ";
-                //echo $del_folder_query;
-                $del_suc1 = $this->getDBResults($del_folder_query);
-            }
+			foreach($del_folders as $eachFolder){
+				$user = explode("-",$eachFolder);
+				$new_folder = ($user[1] == 'private') ? $user[0] : $user[1];
+				$new_folder = $new_folder."-deleted";
+				//escape quotes, make strings query safe
+				$new_folder = $this->db_connection->real_escape_string($new_folder);
+				$eachFolder = $this->db_connection->real_escape_string($eachFolder);
+				$del_folder_query = "update session set `group` = '$new_folder',time = time where `group` = '$eachFolder' ";
+				//echo $del_folder_query;
+				$del_suc1 = $this->getDBResults($del_folder_query);
+			}
 
-            foreach($del_models as $name=>$group){
-                $user = explode("-",$group);
-                $new_folder = ($user[1] == 'private') ? $user[0] : $user[1];
-                $new_folder = $new_folder.'-deleted';
-                $new_folder = $this->db_connection->real_escape_string($new_folder);
-                $name = $this->db_connection->real_escape_string($name);
-                $del_folder_query = "update session set `group` = '$new_folder',time = time where `group` = '$group' and problem='$name' ";
-                echo $del_folder_query;
-                $del_suc2 = $this->getDBResults($del_folder_query);
-            }
-            if($del_suc1 && $del_suc2)
-                return $del_folders;
-            else return null;
+			foreach($del_models as $name=>$group){
+				$user = explode("-",$group);
+				$new_folder = ($user[1] == 'private') ? $user[0] : $user[1];
+				$new_folder = $new_folder.'-deleted';
+				$new_folder = $this->db_connection->real_escape_string($new_folder);
+				$name = $this->db_connection->real_escape_string($name);
+				$del_folder_query = "update session set `group` = '$new_folder',time = time where `group` = '$group' and problem='$name' ";
+				echo $del_folder_query;
+				$del_suc2 = $this->getDBResults($del_folder_query);
+			}
+			if($del_suc1 && $del_suc2)
+				return $del_folders;
+			else return null;
 
-        }
+		}
 
-        function modelAction($parameters){
-            $src = $this->db_connection->real_escape_string($parameters['src']);
-            $model = $this->db_connection->real_escape_string($parameters['mod']);
-            $dest = $this->db_connection->real_escape_string($parameters['dest']);
-            $action = $parameters['action'];
-            $user = $this->db_connection->real_escape_string($parameters['user']);
-            if($action == "moveModel"){
-                //move Model
-                $update_query = "update session set `group` = '$dest',time = time where `group`='$src' AND problem='$model' ";
-                //echo $update_query;
-                $update_res = $this->getDBResults($update_query);
-                if($update_res)
-                    return "success";
-                else
-                    return null;
-            }
-            else if($action == "copyModel"){
-                //step 1 : retrieve the most recent copy of model authored ( session id and other params)
-                $get_q = "select * from session where `group` = '$src' AND problem='$model' AND mode = 'AUTHOR' ORDER BY time DESC limit 1";
-                $res = $this->getDBResults($get_q);
-                if(!$res) {
-                    //echo "session selection failed"."<br/>";
-                    return null;
-                }
-                $data = mysqli_fetch_array($res);
-                $old_session_id = $data["session_id"];
+		function modelAction($parameters){
+			$src = $this->db_connection->real_escape_string($parameters['src']);
+			$model = $this->db_connection->real_escape_string($parameters['mod']);
+			$dest = $this->db_connection->real_escape_string($parameters['dest']);
+			$action = $parameters['action'];
+			$user = $this->db_connection->real_escape_string($parameters['user']);
+			if($action == "moveModel"){
+				//move Model
+				$update_query = "update session set `group` = '$dest',time = time where `group`='$src' AND problem='$model' ";
+				//echo $update_query;
+				$update_res = $this->getDBResults($update_query);
+				if($update_res)
+					return "success";
+				else
+					return null;
+			}
+			else if($action == "copyModel"){
+				//step 1 : retrieve the most recent copy of model authored ( session id and other params)
+				$get_q = "select * from session where `group` = '$src' AND problem='$model' AND mode = 'AUTHOR' ORDER BY time DESC limit 1";
+				$res = $this->getDBResults($get_q);
+				if(!$res) {
+					//echo "session selection failed"."<br/>";
+					return null;
+				}
+				$data = mysqli_fetch_array($res);
+				$old_session_id = $data["session_id"];
 
-                //step 2: copy solution for the old session id which also has to be a fresh insert
-                $get_sol_q = "select * from solutions where session_id='$old_session_id'";
-                $sol_res = $this->getDBResults($get_sol_q);
-                $sol_data = mysqli_fetch_array($sol_res);
-                $solution_graph = $sol_data['solution_graph'];
-                //step 3 : create a new session id with current micro timestamp and session_id
-                //This is work around and in future we need a php session creator from username and section like in js
-                $new_sess1 = explode("_",$old_session_id);
-                $milliseconds = round(microtime(true) * 1000);
-                $new_session_id = $new_sess1[0]."_".$milliseconds;
+				//step 2: copy solution for the old session id which also has to be a fresh insert
+				$get_sol_q = "select * from solutions where session_id='$old_session_id'";
+				$sol_res = $this->getDBResults($get_sol_q);
+				$sol_data = mysqli_fetch_array($sol_res);
+				$solution_graph = $sol_data['solution_graph'];
+				//step 3 : create a new session id with current micro timestamp and session_id
+				//This is work around and in future we need a php session creator from username and section like in js
+				$new_sess1 = explode("_",$old_session_id);
+				$milliseconds = round(microtime(true) * 1000);
+				$new_session_id = $new_sess1[0]."_".$milliseconds;
 
-                //step 4 : insert new session
+				//step 4 : insert new session
 
-                $create_sess_q = "insert into session(`session_id`,`user`,problem,`mode`,`group`,`section`,`activity`)
-                                  VALUES ('$new_session_id','$user','$model','AUTHOR','$dest','non-class-models','construction')";
-                $create_sess_res = $this->getDBResults($create_sess_q);
-                if(!$create_sess_res)
-                    return null;
+				$create_sess_q = "insert into session(`session_id`,`user`,problem,`mode`,`group`,`section`,`activity`)
+								  VALUES ('$new_session_id','$user','$model','AUTHOR','$dest','non-class-models','construction')";
+				$create_sess_res = $this->getDBResults($create_sess_q);
+				if(!$create_sess_res)
+					return null;
 
-                $create_new_sol = "insert into solutions(`session_id`,`share`,`deleted`,`solution_graph`)
-                                   VALUES ('$new_session_id',0,0,'$solution_graph')";
-                $create_sol_res = $this->getDBResults($create_new_sol);
-                //echo $create_new_sol;
-                if($create_sol_res)
-                    return "success";
-                else
-                    return null;
-            }
+				$create_new_sol = "insert into solutions(`session_id`,`share`,`deleted`,`solution_graph`)
+								   VALUES ('$new_session_id',0,0,'$solution_graph')";
+				$create_sol_res = $this->getDBResults($create_new_sol);
+				//echo $create_new_sol;
+				if($create_sol_res)
+					return "success";
+				else
+					return null;
+			}
 
-        }
+		}
+
+		function renameAction($parameters){
+			$old_folder = $this->db_connection->real_escape_string($parameters['old_folder']);
+			$old_model = $this->db_connection->real_escape_string($parameters['old_model']);
+			$new_item = $this->db_connection->real_escape_string($parameters['new_item']);
+			$action = $parameters['action'];
+			if($action == "Folder"){
+				$update_query = "update session set `group` = '$new_item',time = time where `group`='$old_folder'";
+				//echo $update_query;
+				$update_res = $this->getDBResults($update_query);
+				echo $update_query;
+				if($update_res)
+					return "success";
+				else
+					return null;
+			}
+			else if($action == "Model"){
+				$update_query = "update session set problem = '$new_item',time = time where `group`='$old_folder' AND problem='$old_model' ";
+				echo $update_query;
+				$update_res = $this->getDBResults($update_query);
+				if($update_res)
+					return "success";
+				else
+					return null;
+			}
+		}
 
 		function copy_nc_model_section($parameters){
 			$u = $this->db_connection->real_escape_string($parameters['u']);
--- www/global.php
+++ www/global.php
@@ -15,24 +15,24 @@ switch($type){
 		$parameters['g'] = $_REQUEST['g'];
 		$result = $db->getClassProblemGroups($parameters);
 		break;
-    case 'reqNonClassProblems':
-        $parameters['g'] = $_REQUEST['g'];
-        $result = $db->getNonClassProblems($parameters);
-        break;
-    case 'deleteNonClassProblems':
-        $parameters['df'] = $_REQUEST['df'];
-        $parameters['dm'] = $_REQUEST['dm'];
-        $result = $db->deleteNonClassProblems($parameters);
-        break;
-    case 'modelAction':
-        //common for copy and move models
-        $parameters['action'] = $_REQUEST['action'];
-        $parameters['src'] = $_REQUEST['src'];
-        $parameters['mod'] = $_REQUEST['mod'];
-        $parameters['dest'] = $_REQUEST['dest'];
-        $parameters['user'] = $_REQUEST['user'];
-        $result = $db->modelAction($parameters);
-        break;
+	case 'reqNonClassProblems':
+		$parameters['g'] = $_REQUEST['g'];
+		$result = $db->getNonClassProblems($parameters);
+		break;
+	case 'deleteNonClassProblems':
+		$parameters['df'] = $_REQUEST['df'];
+		$parameters['dm'] = $_REQUEST['dm'];
+		$result = $db->deleteNonClassProblems($parameters);
+		break;
+	case 'modelAction':
+		//common for copy and move models
+		$parameters['action'] = $_REQUEST['action'];
+		$parameters['src'] = $_REQUEST['src'];
+		$parameters['mod'] = $_REQUEST['mod'];
+		$parameters['dest'] = $_REQUEST['dest'];
+		$parameters['user'] = $_REQUEST['user'];
+		$result = $db->modelAction($parameters);
+		break;
 	case 'copyNCModelToSection':
 		$parameters['u'] = $_REQUEST['u'];
 		$parameters['p'] = $_REQUEST['p'];
@@ -41,6 +41,16 @@ switch($type){
 		$parameters['m'] = "AUTHOR";
 		$parameters['a'] = "construction";
 		$result = $db->copy_nc_model_section($parameters);
+		break;
+	case 'renameItems':
+		$parameters['action'] = $_REQUEST['action'];
+		$parameters['old_folder'] = $_REQUEST['old_folder'];
+		if(isset($_REQUEST['old_model']))
+			$parameters['old_model'] = $_REQUEST['old_model'];
+		else
+			$parameters['old_model'] = 'undefined';
+		$parameters['new_item'] = $_REQUEST['new_item'];
+		$result = $db->renameAction($parameters);
 		break;
 }
 
