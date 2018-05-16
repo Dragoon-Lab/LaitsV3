88931161e7ab200c74ff4d39bca2228f907efeb4
Jon Wetzel <jon.wetzel@asu.edu>
2018-05-16 15:16:34 -0700

    refactor(xapi): Update myxapi lib

--- www/ET-2.0/lib/myxapiclient.js
+++ www/ET-2.0/lib/myxapiclient.js
@@ -11,10 +11,15 @@ setupConfig = function(err, launchdata, xAPIWrapper) {
       setRegAPIConfig();
       log("--- content statically configured ---");
     }
+    mappingsAndDifficulties = getMappingsAndDifficulties();
+    if(!!mycb){
+      mycb();
+    }
 }
 
-sendKCScore = function(learningResource,problemID,kcNamesAndScores){
-  var vars = document.getElementById("query").value.split('&');
+sendKCScore = function(learningResource,problemID,kcScore){
+  setRegAPIConfig();
+  var vars = window.location.search.substring(1).split('&');
   var params = {}
   for (var i=0;i<vars.length;i++) {
     var pair = vars[i].split("=");
@@ -24,8 +29,18 @@ sendKCScore = function(learningResource,problemID,kcNamesAndScores){
   var fullName = decodeURIComponent(params['fullname']);
   var SKOTitle = learningResource + ":" + problemID;
 
+  var statements = [];
+
+  var kcs = mappingsAndDifficulties['mappings']['itemsToKCs'][problemID];
+  var kcNamesAndScores = {};
+  for(var index in kcs){
+    var kc = kcs[index];
+    kcNamesAndScores[kc] = kcScore;
+  }
+
   for(var kcName in kcNamesAndScores){
     var kcScore = kcNamesAndScores[kcName];
+    var objUUID = ADL.ruuid();
     allData = {
       "actor" :{
         "mbox" : email,
@@ -40,7 +55,7 @@ sendKCScore = function(learningResource,problemID,kcNamesAndScores){
       },
       "object": {
         "objectType": "StatementRef",
-        "id": "80be9dae-f3c4-45ec-841d-1a5a2dcd4def"
+        "id": objUUID
       },
       "context": {
         "extensions": {
@@ -58,49 +73,15 @@ sendKCScore = function(learningResource,problemID,kcNamesAndScores){
       }
     }
 
-    var ret = wrapper.sendStatement(allData);
-  }
-}
-
-sendCompleted = function(learningResource,problemID){
-  var vars = document.getElementById("query").value.split('&');
-  var params = {}
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    params[pair[0]] = pair[1];
-  }
-  var email = "mailto:" + decodeURIComponent(params['user']);
-  var fullName = decodeURIComponent(params['fullname']);
-  var SKOTitle = learningResource + ":" + problemID;
-
-  allData = {
-    "actor" :{
-      "mbox" : email,
-      "name" : fullName,
-      "objectType" : "Agent"
-    },
-    "verb" : {
-      "display": {
-        "en-US": "completed"
-      },
-      "id": "http://adlnet.gov/expapi/verbs/completed"
-    },
-    "object": {
-      "objectType": "StatementRef",
-      "id": "80be9dae-f3c4-45ec-841d-1a5a2dcd4def"
-    },
-    "context": {
-      "extensions": {
-        "http://autotutor&46;x-in-y&46;com/AT": QueryStringToJSON(SKOTitle)
-      }
-    }
+    statements.push(allData);
   }
 
-  var ret = wrapper.sendStatement(allData);
+  var ret = wrapper.sendStatements(statements);
+  return ret;
 }
 
 QueryStringToJSON = function(SKOTitle) {
-	var pairs = document.getElementById("query").value.split('&');
+	var pairs = window.location.href.split('?')[1].split('&');
 
 	var result = {};
 	pairs.forEach(function(pair) {
@@ -116,7 +97,7 @@ QueryStringToJSON = function(SKOTitle) {
 }
 
 qs = function(search_for) {
-	var query = document.getElementById("query").value;
+	var query = window.location.search.substring(1);
 	var params = query.split('&');
 	for (var i = 0; i<params.length; i++) {
 		var pos = params[i].indexOf('=');
@@ -126,24 +107,60 @@ qs = function(search_for) {
 	}
 }
 
+getRandomArrayValue = function(arr){
+  return arr[getRandomNumber(arr.length)];
+}
+
+getRandomNumber = function(max){
+  return Math.floor(Math.random() * max);
+}
+
+toTitleCase = function(str) {
+    return str.replace(/(?:^|\s)\w/g, function(match) {
+        return match.toUpperCase();
+    });
+}
+
+getMappingsAndDifficulties = function(){
+  log("getMappingsAndDifficulties");
+  try{
+    setRegAPIConfig();
+    var search = ADL.XAPIWrapper.searchParams();
+    search['verb'] = "https://umiis.github.io/ITSProfile/System/Store_Data";
+    search['agent'] = JSON.stringify({"mbox":"mailto:data@electronixtutor.com","objectType":"Agent"});
+    search['limit'] = 1;
+    var ret = ADL.XAPIWrapper.getStatements(search);
+    if(ret){
+      var mappingsAndDifficulties = ret['statements'][0]['context']["extensions"]["https://umiis.github.io/ITSProfile/System/Data"];
+      log("mappingsAndDifficulties: " + JSON.stringify(mappingsAndDifficulties));
+      return mappingsAndDifficulties;
+    }
+  }catch(e){
+    log("error getting mappings and difficulties: " + e);
+  }
+}
+
 var setAggAPIConfig = function(){
   wrapper.changeConfig({
-    endpoint: ETConfig.config.aggAPIEndPoint,
-    user: ETConfig.config.user,
-    password: ETConfig.config.password
+    endpoint: "https://lrs.x-in-y.com/api/",
+    user: '17e055b6235655b9893e27fe877fa5f46ef3f6cd',
+    password: '0a0e8ad3df11b173b7eea1593f4ba3cea441429d'
   });
 }
 
 var setRegAPIConfig = function(){
-  
   wrapper.changeConfig({
-    endpoint: ETConfig.config.regAPIEndPoint,
-    user: ETConfig.config.user,
-    password: ETConfig.config.password
+    endpoint: "https://lrs.x-in-y.com/data/xAPI/",
+    user: '17e055b6235655b9893e27fe877fa5f46ef3f6cd',
+    password: '0a0e8ad3df11b173b7eea1593f4ba3cea441429d'
   });
 }
 
 var wrapper;
+var mycb;
+var mappingsAndDifficulties;
 var timeStart = new Date();
-
-ADL.launch(setupConfig, true);
+start = function(cb){
+  mycb = cb;
+  ADL.launch(setupConfig, true);
+}
